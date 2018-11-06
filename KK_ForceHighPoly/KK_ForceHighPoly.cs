a89157d4a1188eceb6d3f07ef05ba2da132e331e
DeathWeasel1337 <37981563+DeathWeasel1337@users.noreply.github.com>
2018-11-06 10:12:15 -0600

    Rewritten to be less crashy. Added config option to disable plugin.

--- KK_ForceHighPoly/KK_ForceHighPoly.cs
+++ KK_ForceHighPoly/KK_ForceHighPoly.cs
@@ -1,22 +1,42 @@
 ﻿using BepInEx;
 using Harmony;
-
+using System;
+using System.ComponentModel;
+/// <summary>
+/// Replaces all _low assets with normal assets, forcing everything to load as high poly
+/// </summary>
 namespace KK_ForceHighPoly
 {
-    [BepInPlugin("com.deathweasel.bepinex.forcehighpoly", "Force High Poly", "1.0")]
+    [BepInPlugin("com.deathweasel.bepinex.forcehighpoly", "Force High Poly", "1.1")]
     public class KK_ForceHighPoly : BaseUnityPlugin
     {
-        public KK_ForceHighPoly()
+        [DisplayName("High poly mode")]
+        [Description("Whether or not to load high poly assets. May require exiting to main menu to take effect.")]
+        public static ConfigWrapper<bool> Enabled { get; private set; }
+
+        void Main()
         {
             var harmony = HarmonyInstance.Create("com.deathweasel.bepinex.forcehighpoly");
             harmony.PatchAll(typeof(KK_ForceHighPoly));
+            Enabled = new ConfigWrapper<bool>("Enabled", "KK_ForceHighPoly", true);
         }
 
         [HarmonyPrefix]
-        [HarmonyPatch(typeof(Manager.Character), nameof(Manager.Character.CreateChara))]
-        public static void CreateCharaPreHook(ref bool hiPoly)
+        [HarmonyBefore(new string[] { "com.bepis.bepinex.resourceredirector" })]
+        [HarmonyPatch(typeof(AssetBundleManager), nameof(AssetBundleManager.LoadAsset), new[] { typeof(string), typeof(string), typeof(Type), typeof(string) })]
+        public static void LoadAssetPrefix(ref string assetName)
         {
-            hiPoly = true;
+            if (Enabled.Value && assetName.EndsWith("_low"))
+                assetName = assetName.Substring(0, assetName.Length - 4);
+        }
+
+        [HarmonyPrefix]
+        [HarmonyBefore(new string[] { "com.bepis.bepinex.resourceredirector" })]
+        [HarmonyPatch(typeof(AssetBundleManager), nameof(AssetBundleManager.LoadAssetAsync), new[] { typeof(string), typeof(string), typeof(Type), typeof(string) })]
+        public static void LoadAssetAsyncPrefix(ref string assetName)
+        {
+            if (Enabled.Value && assetName.EndsWith("_low"))
+                assetName = assetName.Substring(0, assetName.Length - 4);
         }
     }
 }
