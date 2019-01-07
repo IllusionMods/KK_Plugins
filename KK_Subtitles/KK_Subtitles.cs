82069f605aee33943bf14a5cb32eaeb4bc562824
DeathWeasel1337 <DeathWeasel1337@gmail.com>
2019-01-06 23:54:59 -0600

    Adding KK_Subtitles

---
+++ KK_Subtitles/KK_Subtitles.cs
@@ -0,0 +1,128 @@
+﻿using ActionGame.Communication;
+using BepInEx;
+using BepInEx.Logging;
+using Harmony;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using UnityEngine;
+using Logger = BepInEx.Logger;
+
+namespace KK_Subtitles
+{
+    [BepInPlugin(GUID, PluginName, Version)]
+    public class KK_Subtitles : BaseUnityPlugin
+    {
+        public const string PluginName = "KK_Subtitles";
+        public const string GUID = "com.deathweasel.bepinex.subtitles";
+        public const string Version = "1.0";
+        internal static Info ActionGameInfoInstance;
+        public static bool WasTouched = false;
+
+        #region ConfigMgr
+        [DisplayName("Show Untranslated Text")]
+        [Category("Caption Text")]
+        public static ConfigWrapper<bool> showUntranslated { get; private set; }
+        [DisplayName("Font")]
+        [Category("Caption Text")]
+        [Advanced(true)]
+        public static ConfigWrapper<string> fontName { get; private set; }
+        [DisplayName("Size")]
+        [Category("Caption Text")]
+        [Description("Positive values in px, negative values in % of screen size")]
+        [AcceptableValueRange(-100, 300, false)]
+        [Advanced(true)]
+        public static ConfigWrapper<int> fontSize { get; private set; }
+        [DisplayName("Style")]
+        [Category("Caption Text")]
+        [Description("Most available fonts are dynamic, but non-dynamic fonts only support Normal style.")]
+        [Advanced(true)]
+        public static ConfigWrapper<FontStyle> fontStyle { get; private set; }
+        [DisplayName("Alignment")]
+        [Category("Caption Text")]
+        [Advanced(true)]
+        public static ConfigWrapper<TextAnchor> textAlign { get; private set; }
+        [DisplayName("Text Offset")]
+        [Category("Caption Text")]
+        [Description("Padding from bottom of screen")]
+        [AcceptableValueRange(0, 100, false)]
+        [Advanced(true)]
+        public static ConfigWrapper<int> textOffset { get; private set; }
+        [DisplayName("Outline Thickness")]
+        [Category("Caption Text")]
+        [AcceptableValueRange(0, 100, false)]
+        [Advanced(true)]
+        public static ConfigWrapper<int> outlineThickness { get; private set; }
+
+        [DisplayName("Text Color")]
+        [Category("Caption Text")]
+        public static ConfigWrapper<Color> textColor { get; private set; }
+        [DisplayName("Outline Color")]
+        [Category("Caption Text")]
+        public static ConfigWrapper<Color> outlineColor { get; private set; }
+        #endregion
+
+        void Main()
+        {
+            var harmony = HarmonyInstance.Create(GUID);
+            harmony.PatchAll(typeof(KK_Subtitles));
+
+            showUntranslated = new ConfigWrapper<bool>("showUntranslated", PluginName, false);
+            fontName = new ConfigWrapper<string>("fontName", PluginName, "Arial");
+            fontSize = new ConfigWrapper<int>("fontSize", PluginName, -5);
+            fontStyle = new ConfigWrapper<FontStyle>("fontStyle", PluginName, FontStyle.Bold);
+            textAlign = new ConfigWrapper<TextAnchor>("textAlignment", PluginName, TextAnchor.LowerCenter);
+            textOffset = new ConfigWrapper<int>("textOffset", PluginName, 10);
+            outlineThickness = new ConfigWrapper<int>("outlineThickness", PluginName, 2);
+        }
+
+        public void Start()
+        {
+            StartCoroutine(InitAsync());
+        }
+
+        private IEnumerator<WaitWhile> InitAsync()
+        {
+            yield return new WaitWhile(() => Singleton<Manager.Config>.Instance == null);
+
+            string Col2str(Color c) => ColorUtility.ToHtmlStringRGBA(c);
+            Color str2Col(string s) => ColorUtility.TryParseHtmlString("#" + s, out Color c) ? c : Color.clear;
+
+            textColor = new ConfigWrapper<Color>("textColor", PluginName, str2Col, Col2str, Manager.Config.TextData.Font1Color);
+            outlineColor = new ConfigWrapper<Color>("outlineColor", PluginName, str2Col, Col2str, Color.black);
+
+            yield return null;
+        }
+
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(LoadVoice), "Play")]
+        public static void PlayVoice(LoadVoice __instance)
+        {
+            if (__instance.audioSource == null || __instance.audioSource.clip == null || __instance.audioSource.loop)
+                return;
+
+            if (Singleton<HSceneProc>.IsInstance())
+                Caption.DisplayHSubtitle(__instance);
+            else if (ActionGameInfoInstance != null)
+                Caption.DisplayDialogueSubtitle(__instance);
+        }
+
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(Info), "Init")]
+        public static void InfoInit(Info __instance)
+        {
+            Caption.InitGUI();
+            ActionGameInfoInstance = __instance;
+        }
+
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(TalkScene), "TouchFunc")]
+        public static void TouchFunc(TalkScene __instance) => WasTouched = true;
+
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(HVoiceCtrl), "Init")]
+        public static void HVoiceCtrlInit() => Caption.InitGUI();
+    }
+}
+
---
+++ KK_Subtitles/KK_Subtitles_Caption.cs
@@ -0,0 +1,155 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using Harmony;
+using System.Collections.Generic;
+using System.Linq;
+using UniRx;
+using UniRx.Triggers;
+using UnityEngine;
+using UnityEngine.UI;
+using Logger = BepInEx.Logger;
+
+namespace KK_Subtitles
+{
+    public class Caption
+    {
+        public static GameObject Pane { get; internal set; }
+
+        internal static void InitGUI()
+        {
+            if (!(Pane = Pane ?? GameObject.Find("KK_Subtitles_Caption")))
+                Pane = new GameObject("KK_Subtitles_Caption");
+
+            var cscl = Pane.GetComponent<CanvasScaler>() ?? Pane.AddComponent<CanvasScaler>();
+            cscl.uiScaleMode = CanvasScaler.ScaleMode.ScaleWithScreenSize;
+            cscl.referenceResolution = new Vector2(1920, 1080);
+            cscl.matchWidthOrHeight = 0.5f;
+
+            (Pane.GetComponent<Canvas>() ?? Pane.AddComponent<Canvas>()).renderMode = RenderMode.ScreenSpaceOverlay;
+            (Pane.GetComponent<CanvasGroup>() ?? Pane.AddComponent<CanvasGroup>()).blocksRaycasts = false;
+
+            var vlg = Pane.GetComponent<VerticalLayoutGroup>() ?? Pane.AddComponent<VerticalLayoutGroup>();
+            vlg.childControlHeight = vlg.childForceExpandHeight = vlg.childControlWidth = vlg.childForceExpandWidth = false;
+            vlg.childAlignment = TextAnchor.LowerCenter;
+            vlg.padding = new RectOffset(0, 0, 0, KK_Subtitles.textOffset.Value);
+        }
+
+        internal static void DisplayDialogueSubtitle(LoadVoice voice)
+        {
+            if (!KK_Subtitles.WasTouched)
+                return;
+            KK_Subtitles.WasTouched = false;
+
+            string text = "";
+            FindText();
+            void FindText()
+            {
+                foreach (var a in KK_Subtitles.ActionGameInfoInstance.dicTalkInfo)
+                {
+                    foreach (var b in a.Value)
+                    {
+                        foreach (var c in b.Value)
+                        {
+                            foreach (var d in c.Value.Where(x => x is ActionGame.Communication.Info.GenericInfo).Select(x => x as ActionGame.Communication.Info.GenericInfo))
+                            {
+                                text = d.talk.Where(x => x.assetbundle == voice.assetBundleName && x.file == voice.assetName).Select(x => x.text).FirstOrDefault();
+                                if (!text.IsNullOrEmpty())
+                                    return;
+                            }
+                        }
+                    }
+                }
+            }
+            if (text.IsNullOrEmpty())
+                return;
+
+            if (KK_Subtitles.showUntranslated.Value == false)
+                foreach (var x in text.ToList())
+                    if (JPChars.Contains(x))
+                        return;
+
+            //string speaker = voice.voiceTrans.gameObject.GetComponentInParent<ChaControl>().chaFile.parameter.firstname;
+
+            DisplaySubtitle(voice, "", text);
+        }
+
+        internal static void DisplayHSubtitle(LoadVoice voice)
+        {
+            List<HActionBase> lstProc = (List<HActionBase>)AccessTools.Field(typeof(HSceneProc), "lstProc").GetValue(Singleton<HSceneProc>.Instance);
+            HActionBase mode = lstProc[(int)Singleton<HSceneProc>.Instance.flags.mode];
+            HVoiceCtrl voicectrl = (HVoiceCtrl)AccessTools.Field(typeof(HActionBase), "voice").GetValue(mode);
+
+            //At the start of the H scene, all the text was loaded. Look through the loaded stuff and find the text for the current spoken voice.
+            string text = "";
+            FindText();
+            void FindText()
+            {
+                foreach (var a in voicectrl.dicVoiceIntos)
+                {
+                    foreach (var b in a)
+                    {
+                        foreach (var c in b.Value)
+                        {
+                            text = c.Value.Where(x => x.Value.pathAsset == voice.assetBundleName && x.Value.nameFile == voice.assetName).Select(x => x.Value.word).FirstOrDefault();
+                            if (!text.IsNullOrEmpty())
+                                return;
+                        }
+                    }
+                }
+            }
+            if (text.IsNullOrEmpty())
+                return;
+
+            if (KK_Subtitles.showUntranslated.Value == false)
+                foreach (var x in text.ToList())
+                    if (JPChars.Contains(x))
+                        return;
+
+            string speaker = voice.voiceTrans.gameObject.GetComponentInParent<ChaControl>().chaFile.parameter.firstname;
+
+            Logger.Log(LogLevel.Info, text);
+            DisplaySubtitle(voice, speaker, text);
+        }
+
+        internal static void DisplaySubtitle(LoadVoice voice, string speaker, string text)
+        {
+            Font fontFace = (Font)Resources.GetBuiltinResource(typeof(Font), $"{KK_Subtitles.fontName.Value}.ttf");
+            int fsize = KK_Subtitles.fontSize.Value;
+            fsize = (int)(fsize < 0 ? (fsize * Screen.height / -100.0) : fsize);
+
+            GameObject subtitle = new GameObject(voice.assetName);
+            subtitle.transform.SetParent(Pane.transform);
+
+            var rect = subtitle.GetComponent<RectTransform>() ?? subtitle.AddComponent<RectTransform>();
+            rect.pivot = new Vector2(0.5f, 0);
+            rect.sizeDelta = new Vector2(Screen.width * 0.990f, fsize + (fsize * 0.05f));
+
+            var subtitleText = subtitle.GetComponent<Text>() ?? subtitle.AddComponent<Text>();
+            subtitleText.font = fontFace;
+            subtitleText.fontSize = fsize;
+            subtitleText.fontStyle = fontFace.dynamic ? KK_Subtitles.fontStyle.Value : FontStyle.Normal;
+            subtitleText.alignment = KK_Subtitles.textAlign.Value;
+            subtitleText.horizontalOverflow = HorizontalWrapMode.Wrap;
+            subtitleText.verticalOverflow = VerticalWrapMode.Overflow;
+            subtitleText.color = KK_Subtitles.textColor.Value;
+
+            var subOutline = subtitle.GetComponent<Outline>() ?? subtitle.AddComponent<Outline>();
+            subOutline.effectColor = KK_Subtitles.outlineColor.Value;
+            subOutline.effectDistance = new Vector2(KK_Subtitles.outlineThickness.Value, KK_Subtitles.outlineThickness.Value);
+
+            if (speaker.IsNullOrEmpty())
+                subtitleText.text = text;
+            else
+                subtitleText.text = $"{speaker}:{text}";
+            Logger.Log(LogLevel.Info, text);
+
+            voice.OnDestroyAsObservable().Subscribe(delegate (Unit _)
+            {
+                subtitle.transform.SetParent(null);
+                Object.Destroy(subtitle);
+            });
+        }
+
+        private static HashSet<char> JPChars = new HashSet<char>("あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもやゆよらりるれろわんアイウエカキクケコサシスセソタチツテトナニヌネノハヒフヘホマミムメモヤユヨラリルレロワヲン");
+    }
+}
