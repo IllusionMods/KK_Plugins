f1bbe0d2a29968eca732e26212ce55a63cddb1ee
DeathWeasel1337 <DeathWeasel1337@gmail.com>
2019-02-19 23:39:18 -0600

    Now corrects personalities when added by the random button

--- KK_PersonalityCorrector/KK_PersonalityCorrector.cs
+++ KK_PersonalityCorrector/KK_PersonalityCorrector.cs
@@ -2,7 +2,7 @@
 using Harmony;
 using System.Collections.Generic;
 /// <summary>
-/// Changes any story character personalities to the "Pure" personality to prevent the game from breaking when adding them to the class
+/// Changes any invalid personalities to the "Pure" personality to prevent the game from breaking when adding them to the class
 /// </summary>
 namespace KK_PersonalityCorrector
 {
@@ -11,58 +11,68 @@ namespace KK_PersonalityCorrector
     {
         public const string GUID = "com.deathweasel.bepinex.personalitycorrector";
         public const string PluginName = "Personality Corrector";
-        public const string Version = "1.2";
+        public const string Version = "1.3";
         public static int DefaultPersonality = 8; //Pure
 
         void Main()
         {
-            var harmony = HarmonyInstance.Create("com.deathweasel.bepinex.personalitycorrector");
+            var harmony = HarmonyInstance.Create(GUID);
             harmony.PatchAll(typeof(KK_PersonalityCorrector));
         }
 
-        [HarmonyPostfix]
-        [HarmonyPatch(typeof(ActionGame.ClassRoomCharaFile), nameof(ActionGame.ClassRoomCharaFile.InitializeList))]
+        [HarmonyPostfix, HarmonyPatch(typeof(ChaControl), nameof(ChaControl.GetRandomFemaleCard))]
+        public static void GetRandomFemaleCard(ref ChaFileControl[] __result)
+        {
+            foreach (ChaFileControl chaFileControl in __result)
+                CheckPersonalityAndOverride(chaFileControl);
+        }
+
+        [HarmonyPostfix, HarmonyPatch(typeof(ActionGame.ClassRoomCharaFile), nameof(ActionGame.ClassRoomCharaFile.InitializeList))]
         public static void InitializeList(ActionGame.ClassRoomCharaFile __instance)
         {
             Dictionary<int, ChaFileControl> chaFileDic = Traverse.Create(__instance).Field("chaFileDic").GetValue<Dictionary<int, ChaFileControl>>();
 
             foreach (var x in chaFileDic)
+                CheckPersonalityAndOverride(x.Value);
+        }
+
+        private static void CheckPersonalityAndOverride(ChaFileControl chaFileControl)
+        {
+            Logger.Log(BepInEx.Logging.LogLevel.Info, $"{chaFileControl.parameter.fullname.Trim()}:{chaFileControl.parameter.personality}");
+            switch (chaFileControl.parameter.personality)
             {
-                switch (x.Value.parameter.personality)
-                {
-                    case 30: //0727 Free DLC
-                        if (!AssetBundleCheck.IsFile("etcetra/list/config/14.unity3d"))
-                            x.Value.parameter.personality = DefaultPersonality;
-                        break;
-                    case 31: //0727 Paid DLC #1
-                        if (!AssetBundleCheck.IsFile("etcetra/list/config/15.unity3d"))
-                            x.Value.parameter.personality = DefaultPersonality;
-                        break;
-                    case 32: //0727 Paid DLC #1
-                        if (!AssetBundleCheck.IsFile("etcetra/list/config/16.unity3d"))
-                            x.Value.parameter.personality = DefaultPersonality;
-                        break;
-                    case 33: //0727 Paid DLC #1
-                        if (!AssetBundleCheck.IsFile("etcetra/list/config/17.unity3d"))
-                            x.Value.parameter.personality = DefaultPersonality;
-                        break;
-                    case 34:
-                    case 35:
-                    case 36:
-                    case 37: //1221 Paid DLC #2
-                        if (!AssetBundleCheck.IsFile("etcetra/list/config/20.unity3d"))
-                            x.Value.parameter.personality = DefaultPersonality;
-                        break;
-                    case 80:
-                    case 81:
-                    case 82:
-                    case 83:
-                    case 84:
-                    case 85:
-                    case 86: //Story character personalities added by a mod
-                        x.Value.parameter.personality = DefaultPersonality;
-                        break;
-                }
+                case 30: //0727 Free DLC
+                    if (!AssetBundleCheck.IsFile("etcetra/list/config/14.unity3d"))
+                        chaFileControl.parameter.personality = DefaultPersonality;
+                    break;
+                case 31: //0727 Paid DLC #1
+                    if (!AssetBundleCheck.IsFile("etcetra/list/config/15.unity3d"))
+                        chaFileControl.parameter.personality = DefaultPersonality;
+                    break;
+                case 32: //0727 Paid DLC #1
+                    if (!AssetBundleCheck.IsFile("etcetra/list/config/16.unity3d"))
+                        chaFileControl.parameter.personality = DefaultPersonality;
+                    break;
+                case 33: //0727 Paid DLC #1
+                    if (!AssetBundleCheck.IsFile("etcetra/list/config/17.unity3d"))
+                        chaFileControl.parameter.personality = DefaultPersonality;
+                    break;
+                case 34:
+                case 35:
+                case 36:
+                case 37: //1221 Paid DLC #2
+                    if (!AssetBundleCheck.IsFile("etcetra/list/config/20.unity3d"))
+                        chaFileControl.parameter.personality = DefaultPersonality;
+                    break;
+                case 80:
+                case 81:
+                case 82:
+                case 83:
+                case 84:
+                case 85:
+                case 86: //Story character personalities added by a mod
+                    chaFileControl.parameter.personality = DefaultPersonality;
+                    break;
             }
         }
     }
