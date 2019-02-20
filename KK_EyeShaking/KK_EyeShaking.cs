ea2c8de4ba02be122741cb0cb7b14a51a74c0596
DeathWeasel1337 <DeathWeasel1337@gmail.com>
2019-02-20 13:43:30 -0600

    Added KK_EyeShaking

---
+++ KK_EyeShaking/KK_EyeShaking.cs
@@ -0,0 +1,96 @@
+﻿using BepInEx;
+using Harmony;
+using KKAPI;
+using KKAPI.Chara;
+using System.ComponentModel;
+/// <summary>
+/// Adds shaking to a character's eye highlights when she is a virgin in an H scene
+/// </summary>
+namespace KK_EyeShaking
+{
+    [BepInPlugin(GUID, PluginName, Version)]
+    public class KK_EyeShaking : BaseUnityPlugin
+    {
+        public const string GUID = "com.deathweasel.bepinex.eyeshaking";
+        public const string PluginName = "Eye Shaking";
+        public const string PluginNameInternal = "KK_EyeShaking";
+        public const string Version = "1.0";
+        [DisplayName("Enabled")]
+        [Category("Config")]
+        [Description("When enabled, virgins in H scenes will appear to have shaking eye highlights")]
+        public static ConfigWrapper<bool> Enabled { get; private set; }
+
+        void Main()
+        {
+            var harmony = HarmonyInstance.Create(GUID);
+            harmony.PatchAll(typeof(KK_EyeShaking));
+            CharacterApi.RegisterExtraBehaviour<EyeShakingController>(GUID);
+            Enabled = new ConfigWrapper<bool>("Enabled", PluginNameInternal, true);
+        }
+
+        private static EyeShakingController GetController(ChaControl character) => character?.gameObject?.GetComponent<EyeShakingController>();
+
+        public class EyeShakingController : CharaCustomFunctionController
+        {
+            internal bool IsVirgin { get; set; } = true;
+            internal bool IsVirginOrg { get; set; } = true;
+            internal bool IsInit { get; set; } = false;
+
+            protected override void OnCardBeingSaved(GameMode currentGameMode) { }
+            protected override void OnReload(GameMode currentGameMode) { }
+
+            internal void HSceneStart(bool virgin)
+            {
+                IsVirgin = virgin;
+                IsVirginOrg = virgin;
+                IsInit = true;
+            }
+
+            internal void HSceneEnd()
+            {
+                ChaControl.ChangeEyesShaking(false);
+                IsInit = false;
+            }
+
+            internal void OnInsert() => IsVirgin = false;
+            internal void AddOrgasm() => IsVirginOrg = false;
+
+            protected override void Update()
+            {
+                if (Enabled.Value && IsInit && (IsVirgin || IsVirginOrg))
+                    ChaControl.ChangeEyesShaking(true);
+            }
+        }
+        /// <summary>
+        /// Insert vaginal
+        /// </summary>
+        [HarmonyPrefix, HarmonyPatch(typeof(HFlag), nameof(HFlag.AddSonyuKokanPlay))]
+        public static void AddSonyuKokanPlay(HFlag __instance) => GetController(__instance.lstHeroine[0].chaCtrl).OnInsert();
+        /// <summary>
+        /// Insert anal
+        /// </summary>
+        [HarmonyPrefix, HarmonyPatch(typeof(HFlag), nameof(HFlag.AddSonyuAnalPlay))]
+        public static void AddSonyuAnalPlay(HFlag __instance) => GetController(__instance.lstHeroine[0].chaCtrl).OnInsert();
+        /// <summary>
+        /// Something that happens at the end of H scene loading, good enough place to hook
+        /// </summary>
+        [HarmonyPrefix, HarmonyPatch(typeof(HSceneProc), "MapSameObjectDisable")]
+        public static void MapSameObjectDisable(HSceneProc __instance)
+        {
+            SaveData.Heroine heroine = __instance.flags.lstHeroine[0];
+            GetController(heroine.chaCtrl).HSceneStart(heroine.isVirgin && heroine.isAnalVirgin);
+        }
+
+        [HarmonyPrefix, HarmonyPatch(typeof(HFlag), nameof(HFlag.AddSonyuOrg))]
+        public static void AddSonyuOrg(HFlag __instance) => GetController(__instance.lstHeroine[0].chaCtrl).AddOrgasm();
+        [HarmonyPrefix, HarmonyPatch(typeof(HFlag), nameof(HFlag.AddSonyuSame))]
+        public static void AddSonyuSame(HFlag __instance) => GetController(__instance.lstHeroine[0].chaCtrl).AddOrgasm();
+        [HarmonyPrefix, HarmonyPatch(typeof(HFlag), nameof(HFlag.AddSonyuAnalOrg))]
+        public static void AddSonyuAnalOrg(HFlag __instance) => GetController(__instance.lstHeroine[0].chaCtrl).AddOrgasm();
+        [HarmonyPrefix, HarmonyPatch(typeof(HFlag), nameof(HFlag.AddSonyuAnalSame))]
+        public static void AddSonyuAnalSame(HFlag __instance) => GetController(__instance.lstHeroine[0].chaCtrl).AddOrgasm();
+
+        [HarmonyPrefix, HarmonyPatch(typeof(HSceneProc), "EndProc")]
+        public static void EndProc(HSceneProc __instance) => GetController(__instance.flags.lstHeroine[0].chaCtrl).HSceneEnd();
+    }
+}
---
+++ KK_EyeShaking/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("KK_EyeShaking")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("KK_EyeShaking")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7b514b4e-a0bc-4da9-b2a8-0b72515e8033")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion(KK_EyeShaking.KK_EyeShaking.Version)]
+[assembly: AssemblyFileVersion(KK_EyeShaking.KK_EyeShaking.Version)]
