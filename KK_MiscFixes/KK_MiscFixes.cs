6d899c39551460d3cad75d12892c5a7a68379d94
DeathWeasel1337 <DeathWeasel1337@gmail.com>
2019-02-26 13:47:31 -0600

    Speed up classroom list loading

--- KK_MiscFixes/KK_MiscFixes.cs
+++ KK_MiscFixes/KK_MiscFixes.cs
@@ -5,6 +5,8 @@ using System;
 using UniRx;
 using UnityEngine.UI;
 using static ExtensibleSaveFormat.ExtendedSave;
+using ActionGame;
+using Illusion.Game;
 
 namespace KK_MiscFixes
 {
@@ -24,12 +26,12 @@ namespace KK_MiscFixes
         /// Turn off ExtensibleSaveFormat events
         /// </summary>
         [HarmonyPrefix, HarmonyPatch(typeof(FreeHClassRoomCharaFile), "Start")]
-        public static void StartPrefix(FreeHClassRoomCharaFile __instance) => LoadEventsEnabled = false;
+        public static void FreeHClassRoomCharaFileStartPrefix() => LoadEventsEnabled = false;
         /// <summary>
         /// Turn back on ExtensibleSaveFormat events, load a copy of the character with extended data on this time, and use that instead.
         /// </summary>
         [HarmonyPostfix, HarmonyPatch(typeof(FreeHClassRoomCharaFile), "Start")]
-        public static void StartPostfix(FreeHClassRoomCharaFile __instance)
+        public static void FreeHClassRoomCharaFileStartPostfix(FreeHClassRoomCharaFile __instance)
         {
             LoadEventsEnabled = true;
 
@@ -44,6 +46,38 @@ namespace KK_MiscFixes
                 chaFileControl.LoadCharaFile(info.Value.charaFileName, info.Value.parameter.sex, false, true);
 
                 onEnter(chaFileControl);
+            });
+        }
+
+        /// <summary>
+        /// Turn off ExtensibleSaveFormat events
+        /// </summary>
+        [HarmonyPrefix, HarmonyPatch(typeof(ClassRoomCharaFile), "Start")]
+        public static void ClassRoomCharaFileStartPrefix()
+        {
+            Logger.Log(BepInEx.Logging.LogLevel.Info, "ClassRoomCharaFileStartPrefix");
+            LoadEventsEnabled = false;
+        }
+        /// <summary>
+        /// Turn back on ExtensibleSaveFormat events, load a copy of the character with extended data on this time, and use that instead.
+        /// </summary>
+        [HarmonyPostfix, HarmonyPatch(typeof(ClassRoomCharaFile), "Start")]
+        public static void ClassRoomCharaFileStartPostfix(ClassRoomCharaFile __instance)
+        {
+            LoadEventsEnabled = true;
+
+            ReactiveProperty<ChaFileControl> info = Traverse.Create(__instance).Field("info").GetValue<ReactiveProperty<ChaFileControl>>();
+            Button enterButton = Traverse.Create(__instance).Field("enterButton").GetValue<Button>();
+
+            enterButton.onClick.RemoveAllListeners();
+            enterButton.onClick.AddListener(() =>
+            {
+                var onEnter = (Action<ChaFileControl>)AccessTools.Field(typeof(ClassRoomCharaFile), "onEnter").GetValue(__instance);
+                ChaFileControl chaFileControl = new ChaFileControl();
+                chaFileControl.LoadCharaFile(info.Value.charaFileName, info.Value.parameter.sex, false, true);
+
+                onEnter(chaFileControl);
+                Utils.Sound.Play(SystemSE.sel);
             });
         }
     }
