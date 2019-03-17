f21c1bf0b1af63d74cbc2f45c8be4e2c341e6846
DeathWeasel1337 <deathweasel1337@gmail.com>
2019-03-16 21:08:56 -0500

    Update version number

--- KK_UncensorSelector/KK_UncensorSelector.cs
+++ KK_UncensorSelector/KK_UncensorSelector.cs
@@ -29,7 +29,7 @@ namespace KK_UncensorSelector
         public const string GUID = "com.deathweasel.bepinex.uncensorselector";
         public const string PluginName = "Uncensor Selector";
         public const string PluginNameInternal = nameof(KK_UncensorSelector);
-        public const string Version = "3.0";
+        public const string Version = "3.1";
         private const string UncensorKeyRandom = "Random";
         private const string UncensorKeyNone = "None";
         private const string MaleBodyDefaultValue = UncensorKeyRandom;
