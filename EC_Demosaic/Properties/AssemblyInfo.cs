b9dae076b3fc3259efddae3b6fea474327a9b3af
DeathWeasel1337 <deathweasel1337@gmail.com>
2019-04-30 21:08:18 -0500

    Added EC_Demosaic

---
+++ EC_Demosaic/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("EC_Demosaic")]
+[assembly: AssemblyDescription("Demosaic for EmotionCreators")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("https://github.com/DeathWeasel1337/EC_Plugins")]
+[assembly: AssemblyProduct("EC_Demosaic")]
+[assembly: AssemblyCopyright("Copyright © DeathWeasel1337 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("46fcd429-d43b-4e0c-879d-46760202c288")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion(Demosaic.Demosaic.Version)]
+[assembly: AssemblyFileVersion(Demosaic.Demosaic.Version)]
