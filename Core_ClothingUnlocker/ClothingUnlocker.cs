824c8afdb4283b4e0419fa44f392f7d179861adf
Kokaiinum <42316813+Kokaiinum@users.noreply.github.com>
2019-05-24 20:41:58 +0100

    Add files via upload

---
+++ Core_ClothingUnlocker/ClothingUnlocker.cs
@@ -0,0 +1,23 @@
+﻿using BepInEx;
+using Harmony;
+using System.ComponentModel;
+
+namespace ClothingUnlocker {
+    
+    public partial class ClothingUnlocker : BaseUnityPlugin {
+
+        public const string GUID = "com.deathweasel.bepinex.clothingunlocker";
+        public const string PluginName = "Clothing Unlocker";
+        public const string Version = "1.1";
+
+        [HarmonyPostfix, HarmonyPatch(typeof(ListInfoBase), nameof(ListInfoBase.GetInfoInt))]
+        public static void GetInfoIntPostfix(ChaListDefine.KeyType keyType, ref int __result) {
+            if (keyType == ChaListDefine.KeyType.Sex && EnableCrossdressing.Value)
+                __result = 1;
+            if (keyType == ChaListDefine.KeyType.NotBra && EnableBras.Value)
+                __result = 0;
+            if (keyType == ChaListDefine.KeyType.Coordinate && EnableSkirts.Value)
+                __result = 0;
+        }
+    }
+}
