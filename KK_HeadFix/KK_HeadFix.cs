8336214728ca8e2a3971eca1ad74afa52dcde9c3
DeathWeasel1337 <deathweasel1337@gmail.com>
2019-05-24 21:31:37 -0500

    Added KK_HeadFix

---
+++ KK_HeadFix/KK_HeadFix.cs
@@ -0,0 +1,22 @@
+﻿using Harmony;
+using UnityEngine;
+using BepInEx;
+
+namespace KK_HeadFix
+{
+    [BepInPlugin(GUID, PluginName, Version)]
+    public class KK_HeadFix : BaseUnityPlugin
+    {
+        public const string GUID = "com.deathweasel.bepinex.headfix";
+        public const string PluginName = "Head Fix";
+        public const string Version = "1.0";
+        private void Main() => HarmonyInstance.Create(GUID).PatchAll(typeof(KK_HeadFix));
+
+        [HarmonyPostfix, HarmonyPatch(typeof(ChaControl), "GetTexture")]
+        public static void GetTexture(ChaListDefine.CategoryNo type, int id, ChaListDefine.KeyType assetBundleKey, ChaListDefine.KeyType assetKey, string addStr, ChaControl __instance, ref Texture2D __result)
+        {
+            if (__result == null && !addStr.IsNullOrEmpty())
+                __result = Traverse.Create(__instance).Method("GetTexture", new object[] { type, id, assetBundleKey, assetKey, "" }).GetValue() as Texture2D;
+        }
+    }
+}
---
+++ KK_HeadFix/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("KK_HeadFix")]
+[assembly: AssemblyDescription("Head Fix for Koikatsu")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("https://github.com/DeathWeasel1337/KK_Plugins")]
+[assembly: AssemblyProduct("KK_HeadFix")]
+[assembly: AssemblyCopyright("Copyright © DeathWeasel1337 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5500e5e4-1706-4a1e-a918-887c2f364f55")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion(KK_HeadFix.KK_HeadFix.Version)]
+[assembly: AssemblyFileVersion(KK_HeadFix.KK_HeadFix.Version)]
