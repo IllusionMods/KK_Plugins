b38ec469641f22bca8c2dce3bdd9b3e739d84566
DeathWeasel1337 <deathweasel1337@gmail.com>
2019-06-06 13:40:13 -0500

    Added KK_SettingsFix

---
+++ KK_SettingsFix/KK_SettingsFix.cs
@@ -0,0 +1,26 @@
+﻿using BepInEx;
+using System.IO;
+using System.Reflection;
+
+namespace KK_SettingsFix
+{
+    [BepInPlugin(GUID, PluginName, Version)]
+    [BepInProcess("CharaStudio")]
+    public class KK_SettingsFix : BaseUnityPlugin
+    {
+        public const string GUID = "com.deathweasel.bepinex.settingsfix";
+        public const string PluginName = "Settings Fix";
+        public const string Version = "1.0";
+
+        private void Start()
+        {
+            if (!File.Exists("UserData/setup.xml"))
+                return;
+
+            var xmlr = typeof(InitScene).GetMethod("xmlRead", BindingFlags.NonPublic | BindingFlags.Instance);
+            var initScene = gameObject.AddComponent<InitScene>();
+            xmlr.Invoke(initScene, null);
+            DestroyImmediate(initScene);
+        }
+    }
+}
---
+++ KK_SettingsFix/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("KK_SettingsFix")]
+[assembly: AssemblyDescription("Settings Fix for Koikatsu")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("https://github.com/DeathWeasel1337/KK_Plugins")]
+[assembly: AssemblyProduct("KK_SettingsFix")]
+[assembly: AssemblyCopyright("Copyright © DeathWeasel1337 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a3147960-5869-466e-b3f2-fa7e4afc5a76")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion(KK_SettingsFix.KK_SettingsFix.Version)]
+[assembly: AssemblyFileVersion(KK_SettingsFix.KK_SettingsFix.Version)]
