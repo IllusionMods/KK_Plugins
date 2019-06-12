83887068141548f1c0844b05531853628f5a7e94
DeathWeasel1337 <deathweasel1337@gmail.com>
2019-06-11 22:17:50 -0500

    Update for Koikatsu Party

--- KK_HeadFix/KK_HeadFix.cs
+++ KK_HeadFix/KK_HeadFix.cs
@@ -1,6 +1,7 @@
-﻿using Harmony;
+﻿using BepInEx;
+using Harmony;
+using System.Linq;
 using UnityEngine;
-using BepInEx;
 
 namespace KK_HeadFix
 {
@@ -9,10 +10,16 @@ namespace KK_HeadFix
     {
         public const string GUID = "com.deathweasel.bepinex.headfix";
         public const string PluginName = "Head Fix";
-        public const string Version = "1.0";
-        private void Main() => HarmonyInstance.Create(GUID).PatchAll(typeof(KK_HeadFix));
+        public const string Version = "1.1";
 
-        [HarmonyPostfix, HarmonyPatch(typeof(ChaControl), "GetTexture")]
+        private void Main()
+        {
+            var harmony = HarmonyInstance.Create(GUID);
+            var getTextureMethod = typeof(ChaControl).GetMethod("GetTexture", AccessTools.all);
+            if (getTextureMethod.GetParameters().Any(x => x.Name == "addStr"))
+                harmony.Patch(typeof(ChaControl).GetMethod("GetTexture", AccessTools.all), null, new HarmonyMethod(typeof(KK_HeadFix).GetMethod(nameof(GetTexture), AccessTools.all)));
+        }
+
         public static void GetTexture(ChaListDefine.CategoryNo type, int id, ChaListDefine.KeyType assetBundleKey, ChaListDefine.KeyType assetKey, string addStr, ChaControl __instance, ref Texture2D __result)
         {
             if (__result == null && !addStr.IsNullOrEmpty())
