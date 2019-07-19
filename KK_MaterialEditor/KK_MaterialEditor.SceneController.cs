e11ae5a3863658685657e55b7cabd5cc2a7e252b
DeathWeasel1337 <deathweasel1337@gmail.com>
2019-07-19 10:43:29 -0500

    Fix error on resetting scene

--- KK_MaterialEditor/KK_MaterialEditor.SceneController.cs
+++ KK_MaterialEditor/KK_MaterialEditor.SceneController.cs
@@ -95,11 +95,13 @@ namespace KK_MaterialEditor
                         TextureDictionary = MessagePackSerializer.Deserialize<Dictionary<int, byte[]>>((byte[])texDic);
                 }
                 else if (operation == SceneOperationKind.Import)
+                {
                     if (data.data.TryGetValue(nameof(TextureDictionary), out var texDic) && texDic != null)
                         foreach (var x in MessagePackSerializer.Deserialize<Dictionary<int, byte[]>>((byte[])texDic))
                             importDictionary[x.Key] = SetAndGetTextureID(x.Value);
-                    else if (operation == SceneOperationKind.Clear)
-                        return;
+                }
+                else if (operation == SceneOperationKind.Clear)
+                    return;
 
                 if (data.data.TryGetValue(nameof(RendererPropertyList), out var rendererProperties) && rendererProperties != null)
                     foreach (var loadedProperty in MessagePackSerializer.Deserialize<List<RendererProperty>>((byte[])rendererProperties))
