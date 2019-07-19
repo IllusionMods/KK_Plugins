a83ab7989dc155ff74ad5b5fa470b7824d4677fa
DeathWeasel1337 <deathweasel1337@gmail.com>
2019-07-19 10:46:34 -0500

    Refactoring

--- KK_MaterialEditor/KK_MaterialEditor.SceneController.cs
+++ KK_MaterialEditor/KK_MaterialEditor.SceneController.cs
@@ -90,17 +90,15 @@ namespace KK_MaterialEditor
                 var importDictionary = new Dictionary<int, int>();
 
                 if (operation == SceneOperationKind.Load)
-                {
                     if (data.data.TryGetValue(nameof(TextureDictionary), out var texDic) && texDic != null)
                         TextureDictionary = MessagePackSerializer.Deserialize<Dictionary<int, byte[]>>((byte[])texDic);
-                }
-                else if (operation == SceneOperationKind.Import)
-                {
+
+                if (operation == SceneOperationKind.Import)
                     if (data.data.TryGetValue(nameof(TextureDictionary), out var texDic) && texDic != null)
                         foreach (var x in MessagePackSerializer.Deserialize<Dictionary<int, byte[]>>((byte[])texDic))
                             importDictionary[x.Key] = SetAndGetTextureID(x.Value);
-                }
-                else if (operation == SceneOperationKind.Clear)
+
+                if (operation == SceneOperationKind.Clear)
                     return;
 
                 if (data.data.TryGetValue(nameof(RendererPropertyList), out var rendererProperties) && rendererProperties != null)
