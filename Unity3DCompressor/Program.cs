43395b0d218d07dd4a3334a51d4959baac0f64b1
DeathWeasel1337 <deathweasel1337@gmail.com>
2019-08-06 10:36:33 -0500

    Added Unity3DCompressor

---
+++ Unity3DCompressor/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Unity3DCompressor
+{
+    internal class Program
+    {
+        private static readonly RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider();
+
+        private static void Main(string[] args)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("// Drag and drop this .txt file on to SB3UtilityScript.exe");
+            sb.AppendLine("// Get SB3Utility from https://github.com/enimaroah/SB3Utility/releases");
+            sb.AppendLine();
+            sb.AppendLine("LoadPlugin(PluginDirectory+\"UnityPlugin.dll\")");
+            sb.AppendLine();
+
+            bool processedFile = false;
+
+            foreach (string path in args)
+            {
+                if (File.Exists(path) && path.EndsWith(".unity3d", StringComparison.OrdinalIgnoreCase))
+                {
+                    sb.Append(ProcessFile(path));
+                    processedFile = true;
+                }
+                else if (Directory.Exists(path))
+                {
+                    string[] allfiles = Directory.GetFiles(path, "*.unity3d", SearchOption.AllDirectories);
+                    foreach (var x in allfiles)
+                    {
+                        sb.Append(ProcessFile(x));
+                        processedFile = true;
+                    }
+                }
+            }
+
+            if (processedFile)
+            {
+                Console.WriteLine("Press any key to generate output.txt");
+                Console.ReadKey();
+
+                File.WriteAllText(@"output.txt", sb.ToString());
+            }
+            else
+            {
+                Console.WriteLine("Drag and drop a .unity3d file or a directory containing .unity3d files.");
+                Console.WriteLine("Press any key to close.");
+                Console.ReadKey();
+            }
+        }
+
+        private static string ProcessFile(string path)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            var rnbuf = new byte[16];
+            rng.GetBytes(rnbuf);
+            string CAB = "CAB-" + string.Concat(rnbuf.Select((x) => ((int)x).ToString("X2")).ToArray()).ToLower();
+
+            sb.AppendLine($"unityParser4 = OpenUnity3d(path=\"{path}\")");
+            sb.AppendLine("unityEditor4 = Unity3dEditor(parser=unityParser4)");
+            sb.AppendLine("unityEditor4.GetAssetNames(filter=True)");
+            sb.AppendLine($"unityEditor4.RenameCabinet(cabinetIndex=0, name=\"{CAB}\")");
+            sb.AppendLine("unityEditor4.SaveUnity3d(keepBackup=False, backupExtension=\".unit-y3d\", background=False, clearMainAsset=True, pathIDsMode=-1, compressionLevel=2, compressionBufferSize=262144)");
+            sb.AppendLine();
+            return sb.ToString();
+        }
+    }
+}
