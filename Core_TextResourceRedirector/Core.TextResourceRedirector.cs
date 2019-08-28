f826931dfb0822e29ff906457fa9dc2845f29e05
DeathWeasel1337 <deathweasel1337@gmail.com>
2019-08-28 12:10:48 -0500

    Add KK_TextResourceRedirector, HS_TextResourceRedirector

---
+++ Core_TextResourceRedirector/Core.TextResourceRedirector.cs
@@ -0,0 +1,23 @@
+﻿using BepInEx;
+//Adopted from gravydevsupreme's TextResourceRedirector
+namespace KK_Plugins
+{
+    public partial class TextResourceRedirector : BaseUnityPlugin
+    {
+        public const string PluginName = "Text Resource Redirector";
+        public const string GUID = "com.deathweasel.bepinex.textresourceredirector";
+        public const string Version = "1.0";
+
+        private ExcelDataResourceRedirector _excelRedirector;
+
+        private void Awake()
+        {
+            _excelRedirector = new ExcelDataResourceRedirector();
+#if KK
+            _scenarioRedirector = new ScenarioDataResourceRedirector();
+#endif
+
+            enabled = false;
+        }
+    }
+}
---
+++ HS_TextResourceRedirector/HS.TextResourceRedirector.cs
@@ -0,0 +1,12 @@
+﻿using BepInEx;
+
+namespace KK_Plugins
+{
+    [BepInDependency(XUnity.ResourceRedirector.Constants.PluginData.Identifier)]
+    [BepInDependency(XUnity.AutoTranslator.Plugin.Core.Constants.PluginData.Identifier)]
+    [BepInPlugin(GUID, PluginName, Version)]
+    public partial class TextResourceRedirector : BaseUnityPlugin
+    {
+
+    }
+}
