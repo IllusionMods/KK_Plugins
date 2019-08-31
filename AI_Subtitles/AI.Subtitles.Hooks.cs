00435cafaf78447e2bf256c5766d977c8afe90e8
DeathWeasel1337 <deathweasel1337@gmail.com>
2019-08-31 14:36:12 -0500

    Add AI_Subtitles

---
+++ AI_Subtitles/AI.Subtitles.Hooks.cs
@@ -0,0 +1,17 @@
+﻿using HarmonyLib;
+
+namespace KK_Plugins
+{
+    public partial class Subtitles
+    {
+        internal static class Hooks
+        {
+            [HarmonyPostfix, HarmonyPatch(typeof(LoadAudioBase), "Play")]
+            public static void PlayVoice(LoadAudioBase __instance)
+            {
+                if (SubtitleDictionary.TryGetValue(__instance.assetName, out string text))
+                    Caption.DisplaySubtitle(__instance, text);
+            }
+        }
+    }
+}
---
+++ AI_Subtitles/AI.Subtitles.cs
@@ -0,0 +1,28 @@
+﻿using BepInEx;
+using System.IO;
+using System.Reflection;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace KK_Plugins
+{
+    /// <summary>
+    /// Displays subitles on screen for H scenes and in dialogues
+    /// </summary>
+    [BepInPlugin(GUID, PluginName, Version)]
+    public partial class Subtitles : BaseUnityPlugin
+    {
+        private void Main() => LoadSubtitles();
+
+        private void LoadSubtitles()
+        {
+            using (Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream($"{nameof(KK_Plugins)}.Resources.CharaMakerSubs.xml"))
+            using (XmlReader reader = XmlReader.Create(stream))
+            {
+                XDocument doc = XDocument.Load(reader);
+                foreach (var element in doc.Root.Elements("Sub"))
+                    SubtitleDictionary[element.Attribute("Asset").Value] = element.Attribute("Text").Value;
+            }
+        }
+    }
+}
