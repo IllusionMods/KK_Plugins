26e5907b5613f66f54944232a96fbf1ca79741b3
DeathWeasel1337 <deathweasel1337@gmail.com>
2019-11-01 11:42:44 -0500

    Update AssemblyInfo

--- AI_AnimationController/Properties/AssemblyInfo.cs
+++ AI_AnimationController/Properties/AssemblyInfo.cs
@@ -1,16 +1,15 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("AI_AnimationController")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Animation Controller for Koikatsu")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("AI_AnimationController")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("Copyright © DeathWeasel1337 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion(KK_Plugins.KK_AnimationController.Version)]
+[assembly: AssemblyFileVersion(KK_Plugins.KK_AnimationController.Version)]
