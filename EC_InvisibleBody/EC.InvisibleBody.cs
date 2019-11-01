a2c8317e2f443e290acfbde660186019c82edb6e
DeathWeasel1337 <deathweasel1337@gmail.com>
2019-10-31 22:07:28 -0500

    Cleanup

--- EC_InvisibleBody/EC.InvisibleBody.cs
+++ EC_InvisibleBody/EC.InvisibleBody.cs
@@ -5,5 +5,5 @@ namespace KK_Plugins
     [BepInDependency(KKAPI.KoikatuAPI.GUID)]
     [BepInDependency(ExtensibleSaveFormat.ExtendedSave.GUID)]
     [BepInPlugin(GUID, PluginName, Version)]
-    public partial class InvisibleBody { }
+    public partial class InvisibleBody : BaseUnityPlugin { }
 }
--- KK_InvisibleBody/KK.InvisibleBody.cs
+++ KK_InvisibleBody/KK.InvisibleBody.cs
@@ -1,8 +1,5 @@
 ﻿using BepInEx;
-/// <summary>
-/// Sets the selected characters invisible in Studio. Invisible state saves and loads with the scene.
-/// Also sets female characters invisible in H scenes.
-/// </summary>
+
 namespace KK_Plugins
 {
     [BepInDependency(KKAPI.KoikatuAPI.GUID)]
