0fe14403635df9b53daf2bec10cdbe81ee7ad96f
DeathWeasel1337 <deathweasel1337@gmail.com>
2019-11-05 12:43:35 -0600

    Add version dependencies

--- EC_UncensorSelector/EC.UncensorSelector.cs
+++ EC_UncensorSelector/EC.UncensorSelector.cs
@@ -4,8 +4,8 @@ namespace KK_Plugins
 {
     [BepInDependency(Sideloader.Sideloader.GUID)]
     [BepInDependency(ExtensibleSaveFormat.ExtendedSave.GUID)]
-    [BepInDependency(KKAPI.KoikatuAPI.GUID)]
-    [BepInDependency(KoiSkinOverlayX.KoiSkinOverlayMgr.GUID)]
+    [BepInDependency(KKAPI.KoikatuAPI.GUID, "1.9")]
+    [BepInDependency(KoiSkinOverlayX.KoiSkinOverlayMgr.GUID, "5.1")]
     [BepInPlugin(GUID, PluginName, Version)]
     internal partial class UncensorSelector : BaseUnityPlugin { }
 }
--- KK_UncensorSelector/KK.UncensorSelector.cs
+++ KK_UncensorSelector/KK.UncensorSelector.cs
@@ -5,7 +5,7 @@ namespace KK_Plugins
     [BepInDependency(Sideloader.Sideloader.GUID)]
     [BepInDependency(ExtensibleSaveFormat.ExtendedSave.GUID)]
     [BepInDependency(KKAPI.KoikatuAPI.GUID, "1.9")]
-    [BepInDependency(KoiSkinOverlayX.KoiSkinOverlayMgr.GUID)]
+    [BepInDependency(KoiSkinOverlayX.KoiSkinOverlayMgr.GUID, "5.1")]
     [BepInPlugin(GUID, PluginName, Version)]
     internal partial class UncensorSelector : BaseUnityPlugin { }
 }
