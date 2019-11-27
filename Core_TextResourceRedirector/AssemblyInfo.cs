2bf045b0b6a53a3de7f898e5fb037e3f92de7ac2
DeathWeasel1337 <deathweasel1337@gmail.com>
2019-11-27 12:46:58 -0600

    Set up AssemblyInfo

---
+++ Core_TextResourceRedirector/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using KK_Plugins;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle(TextResourceRedirector.PluginNameInternal)]
+[assembly: AssemblyDescription(TextResourceRedirector.PluginNameInternal)]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("https://github.com/DeathWeasel1337/KK_Plugins")]
+[assembly: AssemblyProduct(TextResourceRedirector.PluginNameInternal)]
+[assembly: AssemblyCopyright("Copyright © DeathWeasel1337 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion(TextResourceRedirector.Version)]
+[assembly: AssemblyFileVersion(TextResourceRedirector.Version)]
