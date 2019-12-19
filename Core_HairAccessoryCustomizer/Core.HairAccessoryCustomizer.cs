62f6c9ddd3c993176386a37a385d7cad2fdd7f5a
DeathWeasel1337 <deathweasel1337@gmail.com>
2019-12-18 18:02:12 -0600

    Increment version number

--- Core_HairAccessoryCustomizer/Core.HairAccessoryCustomizer.cs
+++ Core_HairAccessoryCustomizer/Core.HairAccessoryCustomizer.cs
@@ -17,7 +17,7 @@ namespace KK_Plugins
     {
         public const string GUID = "com.deathweasel.bepinex.hairaccessorycustomizer";
         public const string PluginName = "Hair Accessory Customizer";
-        public const string Version = "1.1.2";
+        public const string Version = "1.1.3";
         internal static new ManualLogSource Logger;
 
         internal static bool ReloadingChara = false;
