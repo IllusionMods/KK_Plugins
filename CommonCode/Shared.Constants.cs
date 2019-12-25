681c3c570eca3a9c17bb28b3fe6b94b0616fd45a
DeathWeasel1337 <deathweasel1337@gmail.com>
2019-12-24 22:21:41 -0600

    Add game name constants

--- CommonCode/Shared.Constants.cs
+++ CommonCode/Shared.Constants.cs
@@ -5,14 +5,22 @@
 #if AI
         internal const string GameName = "AI Girl";
         internal const string StudioProcessName = "StudioNEOV2";
+        internal const string MainGameProcessName = "AI-Syoujyo";
 #elif EC
         internal const string GameName = "Emotion Creators";
+        internal const string MainGameProcessName = "EmotionCreators";
 #elif HS
         internal const string GameName = "Honey Select";
-        internal const string StudioProcessName = "StudioNEO";
+        internal const string StudioProcessName = "StudioNEO_64";
+        internal const string MainGameProcessName = "HoneySelect_64";
+        internal const string BattleArenaProcessName = "BattleArena_64";
 #elif KK
         internal const string GameName = "Koikatsu";
         internal const string StudioProcessName = "CharaStudio";
+        internal const string MainGameProcessName = "Koikatu";
+        internal const string MainGameProcessNameSteam = "Koikatsu Party";
+        internal const string VRProcessName = "KoikatsuVR";
+        internal const string VRProcessNameSteam = "Koikatsu Party VR";
 #endif
     }
 }
