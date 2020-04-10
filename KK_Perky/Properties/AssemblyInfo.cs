32aef403fd59abec1cf98bacdd0e3a773e1d09d8
DeathWeasel1337 <deathweasel1337@gmail.com>
2020-04-09 23:14:52 -0500

    Remove KK_Perky

--- KK_Perky/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using KK_Plugins;
-using System.Reflection;
-using static KK_Plugins.Perky;
-
-[assembly: AssemblyTitle(PluginNameInternal)]
-[assembly: AssemblyProduct(PluginNameInternal)]
-[assembly: AssemblyDescription(PluginName + " for " + Constants.GameName)]
-[assembly: AssemblyVersion(Version)]
-[assembly: AssemblyFileVersion(Version)]
