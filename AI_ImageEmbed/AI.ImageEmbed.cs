c3af280e0fc7993445bd1228eed01073e07c022e
DeathWeasel1337 <deathweasel1337@gmail.com>
2020-04-28 12:36:23 -0500

    Ad version dependency

--- AI_ImageEmbed/AI.ImageEmbed.cs
+++ AI_ImageEmbed/AI.ImageEmbed.cs
@@ -2,7 +2,7 @@
 
 namespace KK_Plugins
 {
-    [BepInDependency(MaterialEditor.GUID)]
+    [BepInDependency(MaterialEditor.GUID, "1.10")]
     [BepInPlugin(GUID, PluginName, Version)]
     public partial class ImageEmbed : BaseUnityPlugin
     {
