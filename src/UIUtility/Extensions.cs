baf0410181ccc041026cca7cfa9898b4f424df95
DeathWeasel1337 <deathweasel1337@gmail.com>
2020-05-10 10:29:46 -0500

    Add extensions for setting UI elements without events

--- src/UIUtility/Extensions.cs
+++ src/UIUtility/Extensions.cs
@@ -1,13 +1,36 @@
 ﻿using System;
 using System.Collections;
+using System.Reflection;
 using UnityEngine;
-using UnityEngine.Events;
 using UnityEngine.UI;
 
 namespace UILib
 {
     internal static class Extensions
     {
+        private static readonly MethodInfo toggleSetMethod;
+        private static readonly MethodInfo sliderSetMethod;
+        private static readonly MethodInfo scrollbarSetMethod;
+
+        private static readonly FieldInfo dropdownValueField;
+        private static readonly MethodInfo dropdownRefreshMethod;  // Unity 5.2 <= only
+
+        static Extensions()
+        {
+            // Find the Toggle's set method
+            toggleSetMethod = FindSetMethod(typeof(Toggle));
+
+            // Find the Slider's set method
+            sliderSetMethod = FindSetMethod(typeof(Slider));
+
+            // Find the Scrollbar's set method
+            scrollbarSetMethod = FindSetMethod(typeof(Scrollbar));
+
+            // Find the Dropdown's value field and its' Refresh method
+            dropdownValueField = (typeof(Dropdown)).GetField("m_Value", BindingFlags.NonPublic | BindingFlags.Instance);
+            dropdownRefreshMethod = (typeof(Dropdown)).GetMethod("Refresh", BindingFlags.NonPublic | BindingFlags.Instance);  // Unity 5.2 <= only
+        }
+
         internal static void ExecuteDelayed(this MonoBehaviour self, Action action, int waitCount = 1)
         {
             self.StartCoroutine(ExecuteDelayed_Routine(action, waitCount));
@@ -18,6 +41,83 @@ namespace UILib
             for (int i = 0; i < waitCount; ++i)
                 yield return null;
             action();
+        }
+
+        /// <summary>
+        /// Return the index of the option matching the specified text or -1 if not found
+        /// </summary>
+        public static int OptionIndex(this Dropdown dropdown, string optionText)
+        {
+            for (int i = 0; i < dropdown.options.Count; i++)
+                if (dropdown.options[i].text == optionText)
+                    return i;
+            return -1;
+        }
+
+        /// <summary>
+        /// Returns the text of the item at the specified index or null if option doesn't exist
+        /// </summary>
+        public static string OptionText(this Dropdown dropdown, int itemIndex)
+        {
+            if (itemIndex < dropdown.options.Count)
+                return dropdown.options[itemIndex].text;
+            else
+                return null;
+        }
+
+        /// <summary>
+        /// Set the value of a Toggle
+        /// </summary>
+        /// <param name="sendCallback">Whether to trigger events</param>
+        public static void Set(this Toggle instance, bool value, bool sendCallback = false)
+        {
+            toggleSetMethod.Invoke(instance, new object[] { value, sendCallback });
+        }
+
+        /// <summary>
+        /// Set the value of a Slider
+        /// </summary>
+        /// <param name="sendCallback">Whether to trigger events</param>
+        public static void Set(this Slider instance, float value, bool sendCallback = false)
+        {
+            sliderSetMethod.Invoke(instance, new object[] { value, sendCallback });
+        }
+
+        /// <summary>
+        /// Set the value of a Scrollbar
+        /// </summary>
+        /// <param name="sendCallback">Whether to trigger events</param>
+        public static void Set(this Scrollbar instance, float value, bool sendCallback = false)
+        {
+            scrollbarSetMethod.Invoke(instance, new object[] { value, sendCallback });
+        }
+
+        /// <summary>
+        /// Set the value of a Dropdown
+        /// </summary>
+        /// <param name="sendCallback">Whether to trigger events</param>
+        public static void Set(this Dropdown instance, int value)
+        {
+            dropdownValueField.SetValue(instance, value);
+#if KK
+            dropdownRefreshMethod.Invoke(instance, new object[] { }); // Unity 5.2 <= only
+#else
+            instance.RefreshShownValue(); // Unity 5.3 >= only
+#endif
+        }
+
+        private static MethodInfo FindSetMethod(Type objectType)
+        {
+            var methods = objectType.GetMethods(BindingFlags.NonPublic | BindingFlags.Instance);
+            for (var i = 0; i < methods.Length; i++)
+            {
+                if (methods[i].Name == "Set" && methods[i].GetParameters().Length == 2)
+                {
+                    return methods[i];
+                }
+            }
+
+            return null;
         }
     }
 }
