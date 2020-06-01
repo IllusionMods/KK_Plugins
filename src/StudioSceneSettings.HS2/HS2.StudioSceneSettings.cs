e21b3c69c4da61e2cf1d2dbd61171f796896830b
DeathWeasel1337 <deathweasel1337@gmail.com>
2020-06-01 14:39:10 -0500

    HS2 port

---
+++ src/StudioSceneSettings.HS2/HS2.StudioSceneSettings.cs
@@ -0,0 +1,16 @@
+﻿using BepInEx;
+using KKAPI;
+using KKAPI.Studio.SaveLoad;
+
+namespace KK_Plugins.StudioSceneSettings
+{
+    [BepInProcess(Constants.StudioProcessName)]
+    [BepInDependency(KoikatuAPI.GUID, "1.11")]
+    [BepInPlugin(GUID, PluginName, Version)]
+    public class StudioSceneSettingsPlugin : StudioSceneSettingsCore
+    {
+        public const string PluginNameInternal = "AI_StudioSceneSettings";
+
+        internal void Start() => StudioSaveLoadApi.RegisterExtraBehaviour<SceneController>(GUID);
+    }
+}
