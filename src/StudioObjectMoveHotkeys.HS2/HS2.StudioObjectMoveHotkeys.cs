f3fa885dca105de5b2725bbe3055afacb7900a22
DeathWeasel1337 <deathweasel1337@gmail.com>
2020-06-01 13:46:43 -0500

    HS2 port

---
+++ src/StudioObjectMoveHotkeys.HS2/HS2.StudioObjectMoveHotkeys.cs
@@ -0,0 +1,11 @@
+﻿using BepInEx;
+
+namespace KK_Plugins
+{
+    [BepInProcess(Constants.StudioProcessName)]
+    [BepInPlugin(GUID, PluginName, Version)]
+    public partial class StudioObjectMoveHotkeys : BaseUnityPlugin
+    {
+        public const string PluginNameInternal = "HS2_StudioObjectMoveHotkeys";
+    }
+}
