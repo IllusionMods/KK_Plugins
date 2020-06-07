15dd44335a2369d8831ee51cad3bdc65d4aae103
DeathWeasel1337 <deathweasel1337@gmail.com>
2020-06-07 13:21:49 -0500

    Increment version number

--- src/MaterialEditor.Core/Core.MaterialEditor.cs
+++ src/MaterialEditor.Core/Core.MaterialEditor.cs
@@ -38,7 +38,7 @@ namespace KK_Plugins.MaterialEditor
         /// <summary>
         /// MaterialEditor plugin version
         /// </summary>
-        public const string Version = "2.0";
+        public const string Version = "2.0.1";
         internal static new ManualLogSource Logger;
 
         internal const string FileExt = ".png";
