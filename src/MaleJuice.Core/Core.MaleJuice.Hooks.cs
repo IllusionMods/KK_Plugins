15d4c116b85eb35155124cbde0e075c08aab4e67
DeathWeasel1337 <deathweasel1337@gmail.com>
2020-06-11 14:54:21 -0500

    AI and HS2 version

---
+++ src/MaleJuice.Core/Core.MaleJuice.Hooks.cs
@@ -0,0 +1,97 @@
+﻿using HarmonyLib;
+using Studio;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+#if AI || HS2
+using AIChara;
+#endif
+
+namespace KK_Plugins
+{
+    public partial class MaleJuice
+    {
+        internal static class Hooks
+        {
+            /// <summary>
+            /// Set juice flags for males in Studio
+            /// </summary>
+            [HarmonyPostfix, HarmonyPatch(typeof(OCIChar), nameof(OCIChar.SetSiruFlags))]
+            internal static void SetSiruFlags(ChaFileDefine.SiruParts _parts, byte _state, OCIChar __instance)
+            {
+                if (__instance is OCICharMale charMale)
+#if KK
+                    charMale.male.SetSiruFlags(_parts, _state);
+#else
+                    charMale.male.SetSiruFlag(_parts, _state);
+#endif
+            }
+            /// <summary>
+            /// Get juice flags for males in Studio
+            /// </summary>
+            [HarmonyPostfix, HarmonyPatch(typeof(OCIChar), nameof(OCIChar.GetSiruFlags))]
+            internal static void GetSiruFlags(ChaFileDefine.SiruParts _parts, OCIChar __instance, ref byte __result)
+            {
+                if (__instance is OCICharMale charMale)
+#if KK
+                    __result = charMale.male.GetSiruFlags(_parts);
+#else
+                    __result = charMale.male.GetSiruFlag(_parts);
+#endif
+            }
+            /// <summary>
+            /// Enable the juice section in Studio always, not just for females
+            /// </summary>
+            [HarmonyPostfix, HarmonyPatch(typeof(MPCharCtrl.LiquidInfo), nameof(MPCharCtrl.LiquidInfo.UpdateInfo))]
+            internal static void LiquidInfoUpdateInfo(OCIChar _char, MPCharCtrl.LiquidInfo __instance)
+            {
+                __instance.active = true;
+                __instance.face.select = _char.GetSiruFlags(ChaFileDefine.SiruParts.SiruKao);
+
+#if KK
+                __instance.breast.select = _char.GetSiruFlags(ChaFileDefine.SiruParts.SiruFrontUp);
+                __instance.back.select = _char.GetSiruFlags(ChaFileDefine.SiruParts.SiruBackUp);
+                __instance.belly.select = _char.GetSiruFlags(ChaFileDefine.SiruParts.SiruFrontDown);
+                __instance.hip.select = _char.GetSiruFlags(ChaFileDefine.SiruParts.SiruBackDown);
+#else
+                __instance.breast.select = _char.GetSiruFlags(ChaFileDefine.SiruParts.SiruFrontTop);
+                __instance.back.select = _char.GetSiruFlags(ChaFileDefine.SiruParts.SiruBackTop);
+                __instance.belly.select = _char.GetSiruFlags(ChaFileDefine.SiruParts.SiruFrontBot);
+                __instance.hip.select = _char.GetSiruFlags(ChaFileDefine.SiruParts.SiruBackBot);
+#endif
+            }
+
+#if AI || HS2
+            [HarmonyTranspiler, HarmonyPatch(typeof(ChaControl), "UpdateSiru")]
+            internal static IEnumerable<CodeInstruction> UpdateSiruTranspiler(IEnumerable<CodeInstruction> instructions)
+            {
+                List<CodeInstruction> instructionsList = instructions.ToList();
+
+                //Remove the gender check
+                if (instructionsList[0].opcode == OpCodes.Ldarg_0 && instructionsList[4].opcode == OpCodes.Ret)
+                    for (int i = 4; i >= 0; i--)
+                        instructionsList.RemoveAt(i);
+                else
+                    Logger.LogError($"Unable to patch UpdateSiru");
+
+                return instructionsList;
+            }
+
+            [HarmonyTranspiler, HarmonyPatch(typeof(ChaControl), "UpdateClothesSiru")]
+            internal static IEnumerable<CodeInstruction> UpdateClothesSiruTranspiler(IEnumerable<CodeInstruction> instructions)
+            {
+                List<CodeInstruction> instructionsList = instructions.ToList();
+
+                //Remove the gender check
+                if (instructionsList[0].opcode == OpCodes.Ldarg_0 && instructionsList[4].opcode == OpCodes.Ret)
+                    for (int i = 4; i >= 0; i--)
+                        instructionsList.RemoveAt(i);
+                else
+                    Logger.LogError($"Unable to patch UpdateClothesSiru");
+
+                return instructionsList;
+            }
+#endif
+        }
+    }
+}
---
+++ src/MaleJuice.Core/Core.MaleJuice.cs
@@ -0,0 +1,114 @@
+﻿using BepInEx;
+using BepInEx.Harmony;
+using BepInEx.Logging;
+using KKAPI;
+using KKAPI.Chara;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+#if AI || HS2
+using AIChara;
+#endif
+
+namespace KK_Plugins
+{
+    /// <summary>
+    /// Enables juice textures for male characters
+    /// </summary>
+    public partial class MaleJuice : BaseUnityPlugin
+    {
+        public const string GUID = "com.deathweasel.bepinex.malejuice";
+        public const string PluginName = "Male Juice";
+        public const string Version = "1.2";
+        public const string PluginNameInternal = Constants.Prefix + "_MaleJuice";
+
+#if KK
+        private static Texture LiquidMask = null;
+#else
+        private static Material LiquidMat = null;
+#endif
+
+        internal static new ManualLogSource Logger;
+
+        internal void Main()
+        {
+            Logger = base.Logger;
+
+            StartCoroutine(LoadJuice());
+        }
+
+        private IEnumerator LoadJuice()
+        {
+            yield return null;
+#if HS2
+            yield return null;
+            yield return null;
+            yield return null;
+#endif
+
+            try
+            {
+#if KK
+                //Get the juice texture used by females
+                var mat = CommonLib.LoadAsset<Material>("chara/mm_base.unity3d", "cf_m_body");
+                LiquidMask = mat.GetTexture("_liquidmask");
+#else
+                //Get the juice material used by females
+                LiquidMat = CommonLib.LoadAsset<Material>("chara/oo_base.unity3d", "c_m_liquid_body");
+                LiquidMat.SetFloat("_WeatheringRange1", 0);
+                LiquidMat.SetFloat("_WeatheringRange2", 0);
+                LiquidMat.SetFloat("_WeatheringRange3", 0);
+                LiquidMat.SetFloat("_WeatheringRange4", 0);
+                LiquidMat.SetFloat("_WeatheringRange5", 0);
+                LiquidMat.SetFloat("_WeatheringRange6", 0);
+#endif
+            }
+            catch
+            {
+                Logger.LogError($"Could not load juice textures.");
+            }
+
+#if KK
+            if (LiquidMask == null) yield break;
+#else
+            if (LiquidMat == null) yield break;
+#endif
+
+            CharacterApi.RegisterExtraBehaviour<MaleJuiceCharaController>(GUID);
+            HarmonyWrapper.PatchAll(typeof(Hooks));
+        }
+
+        public class MaleJuiceCharaController : CharaCustomFunctionController
+        {
+            protected override void OnReload(GameMode currentGameMode, bool maintainState)
+            {
+                SetJuice();
+
+                base.OnReload(currentGameMode, maintainState);
+            }
+            protected override void OnCardBeingSaved(GameMode currentGameMode) { }
+            /// <summary>
+            /// Sets the juice textures for any character lacking them, typically males
+            /// </summary>
+            public void SetJuice() => ChaControl.StartCoroutine(_SetJuice());
+            private IEnumerator _SetJuice()
+            {
+                yield return null;
+
+#if KK
+                if (ChaControl.customMatBody.GetTexture("_liquidmask") == null)
+                    ChaControl.customMatBody.SetTexture("_liquidmask", LiquidMask);
+#else
+                if (ChaControl.cmpBody.targetCustom.rendBody.materials.Length == 1)
+                {
+                    List<Material> newMats = new List<Material>();
+                    newMats.Add(ChaControl.cmpBody.targetCustom.rendBody.materials[0]);
+                    newMats.Add(LiquidMat);
+
+                    ChaControl.cmpBody.targetCustom.rendBody.materials = newMats.ToArray();
+                }
+#endif
+            }
+        }
+    }
+}
