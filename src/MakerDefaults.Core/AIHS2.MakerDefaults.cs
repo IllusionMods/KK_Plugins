2f85c8cc246d7e857bcadf9540f26f0cde1f23bf
DeathWeasel1337 <deathweasel1337@gmail.com>
2020-07-14 01:00:58 -0500

    Add MakerDefaults

---
+++ src/MakerDefaults.Core/AIHS2.MakerDefaults.cs
@@ -0,0 +1,109 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using CharaCustom;
+using KKAPI.Maker;
+using System;
+using UnityEngine.UI;
+
+namespace KK_Plugins
+{
+    [BepInPlugin(GUID, PluginName, Version)]
+    public class MakerDefaults : BaseUnityPlugin
+    {
+        public const string GUID = "com.deathweasel.bepinex.makerdefaults";
+        public const string PluginName = "Maker Defaults";
+        public const string PluginNameInternal = Constants.Prefix + "_MakerDefaults";
+        public const string Version = "1.0";
+        internal static new ManualLogSource Logger;
+
+        public static ConfigEntry<GazeDirection> DefaultGazeDirection { get; private set; }
+        public static ConfigEntry<HeadDirection> DefaultHeadDirection { get; private set; }
+        public static ConfigEntry<int> DefaultPose { get; private set; }
+        public static ConfigEntry<int> DefaultEyebrowPattern { get; private set; }
+        public static ConfigEntry<int> DefaultEyePattern { get; private set; }
+        public static ConfigEntry<int> DefaultMouthPattern { get; private set; }
+        public static ConfigEntry<ClothingState> DefaultClothingState { get; private set; }
+        public static ConfigEntry<Background> DefaultBackground { get; private set; }
+
+        internal void Main()
+        {
+            Logger = base.Logger;
+            MakerAPI.MakerFinishedLoading += MakerFinishedLoading;
+            DefaultGazeDirection = Config.Bind("Settings", "Default Gaze Direction", GazeDirection.Camera, new ConfigDescription("Default gaze direction", null, new ConfigurationManagerAttributes { Order = 20 }));
+            DefaultHeadDirection = Config.Bind("Settings", "Default Head Direction", HeadDirection.Pose, new ConfigDescription("Default head direction", null, new ConfigurationManagerAttributes { Order = 19 }));
+            DefaultPose = Config.Bind("Settings", "Default Pose", 1, new ConfigDescription("Default pose", new AcceptableValueRange<int>(0, 1000), new ConfigurationManagerAttributes { Order = 18 }));
+            DefaultEyebrowPattern = Config.Bind("Settings", "Default Eyebrow Pattern", 1, new ConfigDescription("Default eyebrow pattern", new AcceptableValueRange<int>(1, 10), new ConfigurationManagerAttributes { Order = 17 }));
+            DefaultEyePattern = Config.Bind("Settings", "Default Eye Pattern", 1, new ConfigDescription("Default eye pattern", new AcceptableValueRange<int>(1, 9), new ConfigurationManagerAttributes { Order = 16 }));
+            DefaultMouthPattern = Config.Bind("Settings", "Default Mouth Pattern", 1, new ConfigDescription("Default mouth pattern", new AcceptableValueRange<int>(1, 18), new ConfigurationManagerAttributes { Order = 15 }));
+            DefaultClothingState = Config.Bind("Settings", "Default Clothing State", ClothingState.Automatic, new ConfigDescription("Default clothing state", null, new ConfigurationManagerAttributes { Order = 14 }));
+            DefaultBackground = Config.Bind("Settings", "Default Background", Background.Color, new ConfigDescription("Default background type", null, new ConfigurationManagerAttributes { Order = 13 }));
+        }
+
+        private static void MakerFinishedLoading(object sender, EventArgs e)
+        {
+            //Gaze Direction
+            if (DefaultGazeDirection.Value != GazeDirection.Camera)
+                CustomBase.Instance.transform.Find("CanvasDraw/DrawWindow/dwChara/eyelook/items/tgl02").GetComponent<Toggle>().isOn = true;
+
+            //Head Direction
+            if (DefaultHeadDirection.Value != HeadDirection.Pose)
+                CustomBase.Instance.transform.Find("CanvasDraw/DrawWindow/dwChara/necklook/items/tgl02").GetComponent<Toggle>().isOn = true;
+
+            //Pose
+            if (DefaultPose.Value != 1)
+            {
+                var pose = CustomBase.Instance.transform.Find("CanvasDraw/DrawWindow/dwChara/pose/items/inpNo").GetComponent<InputField>();
+                pose.text = DefaultPose.Value.ToString();
+                pose.onEndEdit.Invoke(DefaultPose.Value.ToString());
+            }
+
+            //Eyebrow pattern
+            if (DefaultEyebrowPattern.Value != 1)
+            {
+                var eyebrow = CustomBase.Instance.transform.Find("CanvasDraw/DrawWindow/dwChara/eyebrow/items/inpNo").GetComponent<InputField>();
+                eyebrow.text = DefaultEyebrowPattern.Value.ToString();
+                eyebrow.onEndEdit.Invoke(DefaultEyebrowPattern.Value.ToString());
+            }
+
+            //Eye pattern
+            if (DefaultEyePattern.Value != 1)
+            {
+                var eye = CustomBase.Instance.transform.Find("CanvasDraw/DrawWindow/dwChara/eye/items/inpNo").GetComponent<InputField>();
+                eye.text = DefaultEyePattern.Value.ToString();
+                eye.onEndEdit.Invoke(DefaultEyePattern.Value.ToString());
+            }
+
+            //Mouth pattern
+            if (DefaultMouthPattern.Value != 1)
+            {
+                var mouth = CustomBase.Instance.transform.Find("CanvasDraw/DrawWindow/dwChara/mouth/items/inpNo").GetComponent<InputField>();
+                mouth.text = DefaultMouthPattern.Value.ToString();
+                mouth.onEndEdit.Invoke(DefaultMouthPattern.Value.ToString());
+            }
+
+            //Clothing State
+            switch (DefaultClothingState.Value)
+            {
+                case ClothingState.Clothed:
+                    CustomBase.Instance.transform.Find("CanvasDraw/DrawWindow/dwCoorde/clothes/items/tgl01").GetComponent<Toggle>().isOn = true;
+                    break;
+                case ClothingState.Underwear:
+                    CustomBase.Instance.transform.Find("CanvasDraw/DrawWindow/dwCoorde/clothes/items/tgl02").GetComponent<Toggle>().isOn = true;
+                    break;
+                case ClothingState.Naked:
+                    CustomBase.Instance.transform.Find("CanvasDraw/DrawWindow/dwCoorde/clothes/items/tgl03").GetComponent<Toggle>().isOn = true;
+                    break;
+            }
+
+            //Background
+            if (DefaultBackground.Value != Background.Color)
+                CustomBase.Instance.transform.Find("CanvasDraw/DrawWindow/dwBG/type/items/tgl02").GetComponent<Toggle>().isOn = true;
+        }
+
+        public enum GazeDirection { Camera, Forward }
+        public enum HeadDirection { Pose, Forward }
+        public enum ClothingState { Automatic, Clothed, Underwear, Naked }
+        public enum Background { Image, Color }
+    }
+}
---
+++ src/MakerDefaults.KK/KK.MakerDefaults.cs
@@ -0,0 +1,123 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using ChaCustom;
+using KKAPI.Maker;
+using System;
+using TMPro;
+using UnityEngine.UI;
+
+namespace KK_Plugins
+{
+    [BepInPlugin(GUID, PluginName, Version)]
+    public class MakerDefaults : BaseUnityPlugin
+    {
+        public const string GUID = "com.deathweasel.bepinex.makerdefaults";
+        public const string PluginName = "Maker Defaults";
+        public const string PluginNameInternal = Constants.Prefix + "_MakerDefaults";
+        public const string Version = "1.0";
+        internal static new ManualLogSource Logger;
+
+        public static ConfigEntry<ClothingState> DefaultClothingState { get; private set; }
+        public static ConfigEntry<EyebrowPattern> DefaultEyebrowPattern { get; private set; }
+        public static ConfigEntry<EyePattern> DefaultEyePattern { get; private set; }
+        public static ConfigEntry<float> DefaultEyeOpenness { get; private set; }
+        public static ConfigEntry<bool> DefaultDisableBlinking { get; private set; }
+        public static ConfigEntry<MouthPattern> DefaultMouthPattern { get; private set; }
+        public static ConfigEntry<float> DefaultMouthOpenness { get; private set; }
+        public static ConfigEntry<GazeDirection> DefaultGazeDirection { get; private set; }
+        public static ConfigEntry<HeadDirection> DefaultHeadDirection { get; private set; }
+        public static ConfigEntry<Background> DefaultBackground { get; private set; }
+        public static ConfigEntry<int> DefaultPose { get; private set; }
+
+        internal void Main()
+        {
+            Logger = base.Logger;
+            MakerAPI.MakerFinishedLoading += MakerFinishedLoading;
+            DefaultClothingState = Config.Bind("Settings", "Default Clothing State", ClothingState.Automatic, new ConfigDescription("Default clothing state", null, new ConfigurationManagerAttributes { Order = 20 }));
+            DefaultEyebrowPattern = Config.Bind("Settings", "Default Eyebrow Pattern", EyebrowPattern.Default, new ConfigDescription("Default eyebrow pattern", null, new ConfigurationManagerAttributes { Order = 19 }));
+            DefaultEyePattern = Config.Bind("Settings", "Default Eye Pattern", EyePattern.Default, new ConfigDescription("Default eye pattern", null, new ConfigurationManagerAttributes { Order = 18 }));
+            DefaultEyeOpenness = Config.Bind("Settings", "Default Eye Openness", 1f, new ConfigDescription("Default eye openness", new AcceptableValueRange<float>(0f, 1f), new ConfigurationManagerAttributes { Order = 17 }));
+            DefaultDisableBlinking = Config.Bind("Settings", "Default Disable Blinking", false, new ConfigDescription("Default disable blinking state", null, new ConfigurationManagerAttributes { Order = 16 }));
+            DefaultMouthPattern = Config.Bind("Settings", "Default Mouth Pattern", MouthPattern.Default, new ConfigDescription("Default mouth pattern", null, new ConfigurationManagerAttributes { Order = 15 }));
+            DefaultMouthOpenness = Config.Bind("Settings", "Default Mouth Openness", 0f, new ConfigDescription("Default mouth openness", new AcceptableValueRange<float>(0f, 1f), new ConfigurationManagerAttributes { Order = 14 }));
+            DefaultGazeDirection = Config.Bind("Settings", "Default Gaze Direction", GazeDirection.AtCamera, new ConfigDescription("Default gaze direction", null, new ConfigurationManagerAttributes { Order = 13 }));
+            DefaultHeadDirection = Config.Bind("Settings", "Default Head Direction", HeadDirection.FromAnimation, new ConfigDescription("Default head direction", null, new ConfigurationManagerAttributes { Order = 12 }));
+            DefaultPose = Config.Bind("Settings", "Default Pose", 0, new ConfigDescription("Default pose, number corresponds to the index of the pose in the dropdown list", new AcceptableValueRange<int>(0, 1000), new ConfigurationManagerAttributes { Order = 11 }));
+            DefaultBackground = Config.Bind("Settings", "Default Background", Background.Image, new ConfigDescription("Default background type", null, new ConfigurationManagerAttributes { Order = 10 }));
+        }
+
+        private static void MakerFinishedLoading(object sender, EventArgs e)
+        {
+            //Clothing state
+            switch (DefaultClothingState.Value)
+            {
+                case ClothingState.Clothed:
+                    CustomBase.Instance.transform.Find("FrontUIGroup/CvsDraw/Top/rbClothesState/imgRbCol01").GetComponent<Toggle>().isOn = true;
+                    break;
+                case ClothingState.Underwear:
+                    CustomBase.Instance.transform.Find("FrontUIGroup/CvsDraw/Top/rbClothesState/imgRbCol02").GetComponent<Toggle>().isOn = true;
+                    break;
+                case ClothingState.Naked:
+                    CustomBase.Instance.transform.Find("FrontUIGroup/CvsDraw/Top/rbClothesState/imgRbCol03").GetComponent<Toggle>().isOn = true;
+                    break;
+            }
+
+            //Set eyebrow pattern. 0=Default, 1=Angry, etc.
+            if (DefaultEyebrowPattern.Value != EyebrowPattern.Default)
+                CustomBase.Instance.transform.Find("FrontUIGroup/CvsDraw/Top/grpEyebrowPtn/ddEyebrowPtn").GetComponent<TMP_Dropdown>().value = (int)DefaultEyebrowPattern.Value;
+
+            //Set eye pattern. 0=Default, 1=Closed, etc.
+            if (DefaultEyePattern.Value != EyePattern.Default)
+                CustomBase.Instance.transform.Find("FrontUIGroup/CvsDraw/Top/grpEyesPtn/ddEyesPtn").GetComponent<TMP_Dropdown>().value = (int)DefaultEyePattern.Value;
+
+            //Eye openness
+            if (DefaultEyeOpenness.Value < 0f || DefaultEyeOpenness.Value > 1f)
+                DefaultEyeOpenness.Value = 1f;
+            if (DefaultEyeOpenness.Value != 1f)
+                CustomBase.Instance.transform.Find("FrontUIGroup/CvsDraw/Top/sldEyeOpen/Slider").GetComponent<Slider>().value = DefaultEyeOpenness.Value;
+
+            //Disable blinking
+            if (DefaultDisableBlinking.Value)
+                CustomBase.Instance.transform.Find("FrontUIGroup/CvsDraw/Top/tglBlink/imgTglCol").GetComponent<Toggle>().isOn = DefaultDisableBlinking.Value;
+
+            //Set mouth pattern. 0=Default, 1=Smile, etc.
+            if (DefaultMouthPattern.Value != MouthPattern.Default)
+                CustomBase.Instance.transform.Find("FrontUIGroup/CvsDraw/Top/grpMouthPtn/ddMouthPtn").GetComponent<TMP_Dropdown>().value = (int)DefaultMouthPattern.Value;
+
+            //Mouth open
+            if (DefaultMouthOpenness.Value < 0f || DefaultMouthOpenness.Value > 1f)
+                DefaultMouthOpenness.Value = 1f;
+            if (DefaultMouthOpenness.Value != 0f)
+                CustomBase.Instance.transform.Find("FrontUIGroup/CvsDraw/Top/sldMouthOpen/Slider").GetComponent<Slider>().value = DefaultMouthOpenness.Value;
+
+            //Gaze Direction
+            if (DefaultGazeDirection.Value != GazeDirection.AtCamera)
+                CustomBase.Instance.transform.Find("FrontUIGroup/CvsDraw/Top/grpEyesLookPtn/ddEyesLookPtn").GetComponent<TMP_Dropdown>().value = (int)DefaultGazeDirection.Value;
+
+            //Head Direction
+            if (DefaultHeadDirection.Value != HeadDirection.FromAnimation)
+                CustomBase.Instance.transform.Find("FrontUIGroup/CvsDraw/Top/grpNeckLookPtn/ddNeckLookPtn").GetComponent<TMP_Dropdown>().value = (int)DefaultHeadDirection.Value;
+
+            //Pose
+            if (DefaultPose.Value != 0)
+            {
+                var ddPose = CustomBase.Instance.transform.Find("FrontUIGroup/CvsDraw/Top/grpPose/ddPose").GetComponent<TMP_Dropdown>();
+                if (DefaultPose.Value < ddPose.options.Count)
+                    ddPose.value = DefaultPose.Value;
+            }
+
+            //Background
+            if (DefaultBackground.Value != Background.Image)
+                CustomBase.Instance.transform.Find("FrontUIGroup/CvsDraw/Top/rbBackType/imgRbCol01").GetComponent<Toggle>().isOn = true;
+        }
+
+        public enum ClothingState { Automatic, Clothed, Underwear, Naked }
+        public enum EyebrowPattern { Default, Angry, Worried, Bored, DoubtL, DoubtR, ThinkingL, ThinkingR, FuriousL, FuriousR, Serious, Anxious, Surprised, Disappointed, Smug, WinkingL, WinkingR }
+        public enum EyePattern { Default, Closed, Smiling, HappyClosed, Happy, WinkingL, WinkingR, Pained, Bashful, Angry, Serious, Bored, Awkward, Hate, Thinking, Sad, Crying, Impatient, Disappointed, Worried, Smug, CircleEyes1, CircleEyes2, SpiralEyes, StarPupils, HeartPupils, FieryEyes, CartoonyWink, VerticalLine, CartoonyClosed, HorizontalLine, CartoonyCrying }
+        public enum MouthPattern { Default, Smiling, HappyBroad, HappyModerate, HappySlight, ExcitedBroad, ExcitedModerate, ExcitedSlight, Angry1, Angry2, Serious1, Serious2, Hate, Lonely, Impatient, Dissatisfied, Amazed, Surprised, SurprisedModerate, Smug, Playful, Eating, HoldInMouth, Kiss, TongueOut, SmallA, BigA, SmallI, BigI, SmallU, BigU, SmallE, BigE, SmallO, BigO, SmallN, BigN, Catlike, Triangle, CartoonySmile }
+        public enum GazeDirection { AtCamera, Upward, RightAndUp, Right, DownAndRight, Downward, DownAndLeft, Left, UpAndLeft, AvertGaze }
+        public enum HeadDirection { FromAnimation, AtCamera, Upward, RightAndUp, Right, DownAndRight, Downward, DownAndLeft, Left, UpAndLeft, AvertGaze }
+        public enum Background { Image, Color }
+    }
+}
