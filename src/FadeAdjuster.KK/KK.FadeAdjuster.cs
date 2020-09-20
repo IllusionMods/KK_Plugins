a6dc515b0ca8e17734e7a00f63c75a557227617c
DeathWeasel1337 <deathweasel1337@gmail.com>
2020-09-20 11:55:55 -0500

    Add FadeAdjuster

---
+++ src/FadeAdjuster.KK/KK.FadeAdjuster.cs
@@ -0,0 +1,51 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using HarmonyLib;
+using UnityEngine;
+
+namespace KK_Plugins
+{
+    /// <summary>
+    /// Adjust fade color or disable it
+    /// </summary>
+    [BepInPlugin(GUID, PluginName, Version)]
+    public class FadeAdjuster : BaseUnityPlugin
+    {
+        public const string GUID = "com.deathweasel.bepinex.fadeadjuster";
+        public const string PluginName = "Fade Adjuster";
+        public const string PluginNameInternal = "KK_FadeAdjuster";
+        public const string Version = "1.0";
+
+        public static ConfigEntry<bool> DisableFade { get; private set; }
+        public static ConfigEntry<Color> FadeColor { get; private set; }
+
+        internal void Awake()
+        {
+            DisableFade = Config.Bind("Config", "Disable Fade", false, "Disables fade on loading screens, may reveal ugly things not meant to be seen");
+            FadeColor = Config.Bind("Config", "Fade Color", Color.white, "Color of the fade for loading screens, requires game restart to take effect");
+
+            Harmony.CreateAndPatchAll(typeof(Hooks));
+        }
+
+        internal static class Hooks
+        {
+            [HarmonyPostfix, HarmonyPatch(typeof(Manager.Scene), "Awake")]
+            internal static void Awake(ref Color ___initFadeColor)
+            {
+                if (FadeColor.Value != Color.white)
+                    ___initFadeColor = FadeColor.Value;
+            }
+
+            [HarmonyPrefix, HarmonyPatch(typeof(SimpleFade), "Update")]
+            internal static bool SimpleFadeUpdate(SimpleFade __instance)
+            {
+                if (DisableFade.Value)
+                {
+                    __instance.ForceEnd();
+                    return false;
+                }
+                return true;
+            }
+        }
+    }
+}
---
+++ src/FadeAdjuster.KK/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using KK_Plugins;
+using System.Reflection;
+using static KK_Plugins.FadeAdjuster;
+
+[assembly: AssemblyTitle(PluginNameInternal)]
+[assembly: AssemblyProduct(PluginNameInternal)]
+[assembly: AssemblyDescription(PluginName + " for " + Constants.GameName)]
+[assembly: AssemblyVersion(Version)]
+[assembly: AssemblyFileVersion(Version)]
