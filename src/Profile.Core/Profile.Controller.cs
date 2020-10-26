1f967660b76f1e7092a503033900a69bd34ed8cc
DeathWeasel1337 <deathweasel1337@gmail.com>
2020-10-25 19:02:26 -0500

    Add Profile plugin

---
+++ src/Profile.Core/Profile.Controller.cs
@@ -0,0 +1,34 @@
+﻿using ExtensibleSaveFormat;
+using KKAPI;
+using KKAPI.Chara;
+using KKAPI.Maker;
+
+namespace KK_Plugins
+{
+    public class ProfileController : CharaCustomFunctionController
+    {
+        string ProfileText;
+
+        protected override void OnCardBeingSaved(GameMode currentGameMode)
+        {
+            if (MakerAPI.InsideAndLoaded)
+                ProfileText = Profile.ProfileTextbox.Value;
+
+            var data = new PluginData();
+            data.data.Add(nameof(ProfileText), ProfileText);
+            SetExtendedData(data);
+        }
+
+        protected override void OnReload(GameMode currentGameMode, bool maintainState)
+        {
+            ProfileText = null;
+
+            var data = GetExtendedData();
+            if (data != null && data.data.TryGetValue(nameof(ProfileText), out var loadedProfileText))
+                ProfileText = loadedProfileText.ToString();
+
+            if (MakerAPI.InsideMaker)
+                Profile.ProfileTextbox.Value = ProfileText ?? "";
+        }
+    }
+}
---
+++ src/Profile.Core/Profile.cs
@@ -0,0 +1,66 @@
+﻿using BepInEx;
+using KKAPI.Chara;
+using KKAPI.Maker;
+using KKAPI.Maker.UI;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace KK_Plugins
+{
+    [BepInPlugin(GUID, PluginName, Version)]
+    public class Profile : BaseUnityPlugin
+    {
+        public const string GUID = "com.deathweasel.bepinex.profile";
+        public const string PluginName = "Profile";
+        public const string PluginNameInternal = Constants.Prefix + "_Profile";
+        public const string Version = "1.0";
+
+        internal static MakerTextbox ProfileTextbox;
+
+        internal void Main()
+        {
+            CharacterApi.RegisterExtraBehaviour<ProfileController>(PluginNameInternal);
+            MakerAPI.RegisterCustomSubCategories += MakerAPI_RegisterCustomSubCategories;
+            MakerAPI.MakerFinishedLoading += MakerAPI_MakerFinishedLoading;
+        }
+
+        private void MakerAPI_MakerFinishedLoading(object sender, System.EventArgs e)
+        {
+            //Allow multi line text, rich text
+            var inputfield = ProfileTextbox.ControlObject.GetComponentInChildren<TMP_InputField>();
+            inputfield.lineType = TMP_InputField.LineType.MultiLineNewline;
+            inputfield.richText = true;
+            inputfield.scrollSensitivity = 10;
+
+            foreach (var text in inputfield.GetComponentsInChildren<TextMeshProUGUI>())
+            {
+                text.alignment = TextAlignmentOptions.TopLeft;
+                text.fontSize = 22;
+            }
+
+            //Extend the background window
+            ProfileTextbox.ControlObject.GetComponent<LayoutElement>().minHeight = 910;
+
+            //Extend the textbox
+            var rect = inputfield.GetComponent<RectTransform>();
+            rect.offsetMax = new Vector2(rect.offsetMax.y, rect.offsetMax.y);
+            rect.sizeDelta = new Vector2(460f, rect.sizeDelta.y);
+
+            //Hide the reset button since that's a recipe for disaster
+            var button = ProfileTextbox.ControlObject.GetComponentInChildren<Button>().gameObject;
+            button.SetActive(false);
+        }
+
+        private void MakerAPI_RegisterCustomSubCategories(object sender, RegisterSubCategoriesEvent e)
+        {
+            MakerCategory category = new MakerCategory(MakerConstants.Parameter.ADK.CategoryName, "Profile");
+            e.AddSubCategory(category);
+            e.AddControl(new MakerText("Character Description", category, this));
+            ProfileTextbox = e.AddControl(new MakerTextbox(category, "", "", this));
+
+            //Starts throwing errors at some point after 30,000 characters
+            ProfileTextbox.CharacterLimit = 30000;
+        }
+    }
+}
