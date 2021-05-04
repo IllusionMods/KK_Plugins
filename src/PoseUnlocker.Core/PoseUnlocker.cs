a67f56c6abf4f98e4ee429c6956876a95be27667
DeathWeasel1337 <deathweasel1337@gmail.com>
2021-05-03 22:39:47 -0500

    Add PoseUnlocker

---
+++ src/PoseUnlocker.Core/PoseUnlocker.cs
@@ -0,0 +1,42 @@
+﻿using BepInEx;
+using HarmonyLib;
+using System.IO;
+
+namespace KK_Plugins
+{
+    [BepInProcess(Constants.StudioProcessName)]
+    [BepInPlugin(GUID, PluginName, Version)]
+    public class PoseUnlocker : BaseUnityPlugin
+    {
+        public const string GUID = "com.deathweasel.bepinex.poseunlocker";
+        public const string PluginName = "Pose Gender Restriction Unlocker";
+        public const string PluginNameInternal = Constants.Prefix + "_PoseUnlocker";
+        public const string Version = "1.0";
+
+        internal void Main()
+        {
+            Harmony.CreateAndPatchAll(typeof(Hooks));
+        }
+
+        private static class Hooks
+        {
+            /// <summary>
+            /// Same as vanilla but without the check against sex
+            /// </summary>
+            [HarmonyPrefix, HarmonyPatch(typeof(Studio.PauseCtrl), nameof(Studio.PauseCtrl.CheckIdentifyingCode))]
+            private static bool CheckIdentifyingCode(string _path, ref bool __result)
+            {
+                using (FileStream input = new FileStream(_path, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+                using (BinaryReader binaryReader = new BinaryReader(input))
+                    if (string.Compare(binaryReader.ReadString(), "【pose】") != 0)
+                    {
+                        __result = false;
+                        return false;
+                    }
+
+                __result = true;
+                return false;
+            }
+        }
+    }
+}
