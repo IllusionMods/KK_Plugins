8b6473967bc91e42d7d0f1c4f55114a1e4c05ae7
DeathWeasel1337 <deathweasel1337@gmail.com>
2021-05-11 09:21:37 -0500

    Increment version number

--- src/MaterialEditor.Core/Core.MaterialEditor.cs
+++ src/MaterialEditor.Core/Core.MaterialEditor.cs
@@ -55,7 +55,7 @@ namespace KK_Plugins.MaterialEditor
         /// <summary>
         /// MaterialEditor plugin version
         /// </summary>
-        public const string PluginVersion = "3.0.4";
+        public const string PluginVersion = "3.0.5";
 
 #if KK || EC
         internal static ConfigEntry<bool> RimRemover { get; private set; }
