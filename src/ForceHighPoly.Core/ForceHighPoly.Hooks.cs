72822f4e0cb8df67092113cca95dca29f3ece668
DeathWeasel1337 <deathweasel1337@gmail.com>
2021-08-28 09:42:11 -0500

    Fix high poly breaking special H modes

--- src/ForceHighPoly.Core/ForceHighPoly.Hooks.cs
+++ src/ForceHighPoly.Core/ForceHighPoly.Hooks.cs
@@ -8,17 +8,28 @@ namespace KK_Plugins
         internal static class Hooks
         {
             [HarmonyPrefix, HarmonyPatch(typeof(AssetBundleManager), nameof(AssetBundleManager.LoadAsset), typeof(string), typeof(string), typeof(Type), typeof(string))]
-            private static void LoadAssetPrefix(ref string assetName)
-            {
-                if (Enabled.Value && assetName.EndsWith("_low"))
-                    assetName = assetName.Substring(0, assetName.Length - 4);
-            }
+            private static void LoadAssetPrefix(ref string assetName) => RemoveLow(ref assetName);
 
             [HarmonyPrefix, HarmonyPatch(typeof(AssetBundleManager), nameof(AssetBundleManager.LoadAssetAsync), typeof(string), typeof(string), typeof(Type), typeof(string))]
-            private static void LoadAssetAsyncPrefix(ref string assetName)
+            private static void LoadAssetAsyncPrefix(ref string assetName) => RemoveLow(ref assetName);
+
+            /// <summary>
+            /// Strip "_low" from the end of an asset name
+            /// </summary>
+            private static void RemoveLow(ref string assetName)
             {
-                if (Enabled.Value && assetName.EndsWith("_low"))
-                    assetName = assetName.Substring(0, assetName.Length - 4);
+                if (Enabled.Value)
+                {
+                    if (assetName.EndsWith("_low"))
+                    {
+#if KKS
+                        if (assetName != "p_cf_body_00_hit_low") //Loading the high poly version of this breaks special H modes
+#endif
+                        {
+                            assetName = assetName.Substring(0, assetName.Length - 4);
+                        }
+                    }
+                }
             }
         }
     }
