dfb1f30bce86935ad2ca05a59503af79871ee0e8
ManlyMarco <dummyaccx@gmail.com>
2021-09-24 18:42:32 +0200

    Add KKS.StudioImageEmbed

--- src/StudioImageEmbed.Core/Core.StudioImageEmbed.Constants.cs
+++ src/StudioImageEmbed.Core/Core.StudioImageEmbed.Constants.cs
@@ -26,6 +26,17 @@ namespace KK_Plugins
         {
             "koi_studio_frame_00.png", "koi_studio_frame_01.png", "koi_studio_frame_02.png", "koi_studio_frame_03.png", "koi_studio_frame_04.png", "koi_studio_frame_05.png", "koi_studio_frame_06.png"
         };
+#elif KKS
+        internal static readonly List<string> DefaultBGs = new List<string>
+        {
+            "bg_01.png", "bg_02.png", "bg_03.png", "bg_04.png", "bg_05.png", "bg_06.png", "bg_07.png", "bg_08.png", "bg_09.png", "bg_10.png", "bg_21.png", "bg_22.png", 
+            "bg_23.png", "bg_24.png", "bg_25.png", "bg_26.png", "bg_27.png", "bg_28.png", "bg_29.png", "bg_30.png", "bg_31.png", 
+            "宇宙空間01.png", "宇宙空間02.png", "宇宙空間03.png", "宇宙空間04.png",
+        };
+        internal static readonly List<string> DefaultFrames = new List<string>
+        {
+            "koi_studio_frame_00.png", "koi_studio_frame_01.png", "koi_studio_frame_02.png", "koi_studio_frame_03.png", "koi_studio_frame_04.png", "koi_studio_frame_05.png", "koi_studio_frame_06.png"
+        };
 #endif
     }
 }
