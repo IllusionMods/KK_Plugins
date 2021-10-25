da5fa6caaacf1a51728228105376465f4bfba46d
ManlyMarco <39247311+ManlyMarco@users.noreply.github.com>
2021-10-25 03:11:06 +0200

    Add KKS.CharaMakerLoadedSound

---
+++ src/CharaMakerLoadedSound.KKS/KKS.CharaMakerLoadedSound.cs
@@ -0,0 +1,23 @@
+﻿using BepInEx;
+using Illusion.Game;
+using KKAPI;
+
+namespace KK_Plugins
+{
+    /// <summary>
+    /// When Chara Maker starts, wait a bit for lag to stop then play a sound
+    /// </summary>
+    [BepInDependency(KoikatuAPI.GUID, KoikatuAPI.VersionConst)]
+    [BepInPlugin(GUID, PluginName, Version)]
+    [BepInProcess(KoikatuAPI.GameProcessName)]
+    //[BepInProcess(KoikatuAPI.GameProcessNameSteam)]
+    public class CharaMakerLoadedSound : BaseUnityPlugin
+    {
+        public const string GUID = "com.deathweasel.bepinex.charamakerloadedsound";
+        public const string PluginName = "Character Maker Loaded Sound";
+        public const string PluginNameInternal = Constants.Prefix + "_CharaMakerLoadedSound";
+        public const string Version = "1.0";
+
+        internal void Main() => KKAPI.Maker.MakerAPI.MakerFinishedLoading += (s, e) => Utils.Sound.Play(SystemSE.ok_s);
+    }
+}
