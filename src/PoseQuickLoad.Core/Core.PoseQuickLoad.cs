0b8a98d6cfd2942abb1cb51be3a4aa1adadc63f6
DeathWeasel1337 <deathweasel1337@gmail.com>
2022-02-16 22:03:51 -0600

    Add quick loading with shift

--- src/PoseQuickLoad.Core/Core.PoseQuickLoad.cs
+++ src/PoseQuickLoad.Core/Core.PoseQuickLoad.cs
@@ -3,6 +3,7 @@ using BepInEx.Configuration;
 using BepInEx.Logging;
 using HarmonyLib;
 using Studio;
+using UnityEngine;
 
 namespace KK_Plugins
 {
@@ -13,17 +14,19 @@ namespace KK_Plugins
         public const string GUID = "com.deathweasel.bepinex.posequickload";
         public const string PluginName = "Pose Quick Load";
         public const string PluginNameInternal = Constants.Prefix + "_PoseQuickLoad";
-        public const string Version = "1.0";
+        public const string Version = "1.1";
         internal static new ManualLogSource Logger;
 
-        public static ConfigEntry<bool> Enabled { get; private set; }
+        public static ConfigEntry<QuickLoad> ConfigPoseQuickLoad { get; private set; }
 
         internal void Main()
         {
             Logger = base.Logger;
-            Enabled = Config.Bind("Config", "Pose Quick Loading", false, "Whether poses in Studio will be loaded by clicking on them. Vanilla behavior requires you to select the pose and then press load.");
+            ConfigPoseQuickLoad = Config.Bind("Config", "Pose Quick Loading", QuickLoad.EnabledWithShift, "Whether poses in Studio will be loaded by clicking on them. Vanilla behavior requires you to select the pose and then press load.");
             Harmony.CreateAndPatchAll(typeof(Hooks));
         }
+
+        public enum QuickLoad { Disabled, Enabled, EnabledWithShift }
     }
 
     internal static class Hooks
@@ -31,8 +34,15 @@ namespace KK_Plugins
         [HarmonyPostfix, HarmonyPatch(typeof(PauseRegistrationList), nameof(PauseRegistrationList.OnClickSelect))]
         private static void OnClickSelect(PauseRegistrationList __instance)
         {
-            if (PoseQuickLoad.Enabled.Value)
+            if (PoseQuickLoad.ConfigPoseQuickLoad.Value == PoseQuickLoad.QuickLoad.Enabled)
+            {
                 __instance.OnClickLoad();
+            }
+            else if (PoseQuickLoad.ConfigPoseQuickLoad.Value == PoseQuickLoad.QuickLoad.EnabledWithShift)
+            {
+                if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+                    __instance.OnClickLoad();
+            }
         }
     }
 }
