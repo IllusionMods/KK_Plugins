83896e5a753de5a64bb6750e9ab0f82f4b441388
jalil <63772680+jalil49@users.noreply.github.com>
2022-04-15 13:04:14 -0500

    Removed notes as requested
    Changed to enum

--- src/ForceHighPoly.Core/ForceHighPoly.Hooks.cs
+++ src/ForceHighPoly.Core/ForceHighPoly.Hooks.cs
@@ -14,10 +14,13 @@ namespace KK_Plugins
             [HarmonyPostfix, HarmonyPatch(typeof(ChaControl), nameof(ChaControl.Initialize))]
             internal static void CheckHiPoly(ChaControl __instance)
             {
+                if (PolySetting.Value == PolyMode.Full) __instance.hiPoly = true;
+
+                if (__instance.hiPoly || PolySetting.Value != PolyMode.Partial) return;
+
                 var exType = Traverse.Create(__instance).Property("exType");
                 var exTypeExists = exType.PropertyExists();
-                if (Enabled.Value) __instance.hiPoly = true;
-                if (__instance.hiPoly || !PartialPoly.Value) return;
+
                 var coordinate = __instance.chaFile.coordinate;
                 for (var i = 0; i < coordinate.Length; i++)
                 {
@@ -38,10 +41,8 @@ namespace KK_Plugins
                                 break;
                         }
                         category += j;
-                        var currentcloth = clothParts[j];
-                        var id = currentcloth.id;
                         var work = __instance.lstCtrl.GetCategoryInfo((ChaListDefine.CategoryNo)category);
-                        if (!work.TryGetValue(id, out var lib))
+                        if (!work.TryGetValue(clothParts[j].id, out var lib))
                         {
                             continue;
                         }
@@ -50,7 +51,7 @@ namespace KK_Plugins
                             var infoInt = lib.GetInfoInt(ChaListDefine.KeyType.Sex);
                             if (__instance.sex == 0 && infoInt == 3 || __instance.sex == 1 && infoInt == 2)
                             {
-                                if (id != 0)
+                                if (clothParts[j].id != 0)
                                 {
                                     work.TryGetValue(0, out lib);
                                 }
@@ -91,134 +92,6 @@ namespace KK_Plugins
             {
                 __result = __result || Enabled.Value;
             }
-            #region Transpile code that works, but animation locks
-            //public static IEnumerable<CodeInstruction> TopAsyncTranspiler(IEnumerable<CodeInstruction> instructions)
-            //{
-            //    var loadObjThis = new CodeInstruction(OpCodes.Ldarg_0, null);
-            //    var AnonType = AccessTools.TypeByName("ChaControl+<ChangeClothesTopAsync>c__Iterator8, Assembly-CSharp");
-            //    var controlThis = new CodeInstruction(OpCodes.Ldfld, AnonType.GetField("$this", AccessTools.all));
-            //    return AsyncTranspiler(instructions, new CodeInstruction[]
-            //    {
-            //        //load chacontrol
-            //        loadObjThis,
-            //        controlThis,
-            //        //load Categories
-            //        new CodeInstruction(OpCodes.Ldc_I4, 105),
-            //        new CodeInstruction(OpCodes.Ldc_I4, 503),
-            //        //load id
-            //        loadObjThis,
-            //        new CodeInstruction(OpCodes.Ldfld, AnonType.GetField("id", AccessTools.all)),
-            //        //load objname
-            //        loadObjThis,
-            //        new CodeInstruction(OpCodes.Ldfld, AnonType.GetField("<objName>__0", AccessTools.all)),
-            //        //load copyDynamicBones
-            //        new CodeInstruction(OpCodes.Ldc_I4_1, null),
-            //        //load copyWeights
-            //        new CodeInstruction(OpCodes.Ldc_I4_1, null),
-            //        //load trfParent
-            //        loadObjThis,
-            //        controlThis,
-            //        new CodeInstruction(OpCodes.Call, AccessTools.PropertyGetter(typeof(ChaInfo),nameof(ChaInfo.objTop))),
-            //        new CodeInstruction(OpCodes.Callvirt, AccessTools.PropertyGetter(typeof(GameObject),nameof(GameObject.transform))),
-            //        //load default
-            //        new CodeInstruction(OpCodes.Ldc_I4_0, null),
-            //        //load async
-            //        loadObjThis,
-            //        new CodeInstruction(OpCodes.Ldfld, AnonType.GetField("asyncFlags", AccessTools.all)),
-
-            //        //load worldpos
-            //        new CodeInstruction(OpCodes.Ldc_I4_0, null),
-            //        //load kindno
-            //        new CodeInstruction(OpCodes.Ldc_I4_0, null),
-            //        //call custom method
-            //        new CodeInstruction(OpCodes.Call, typeof(Hooks).GetMethod(nameof(LoadFbxDual), AccessTools.all)),
-            //        //loadObjThis
-            //        }, AnonType); ;
-            //}
-
-            //public static IEnumerable<CodeInstruction> AsyncTranspiler(IEnumerable<CodeInstruction> instructions, CodeInstruction[] insertInstructions, Type anonType)
-            //{
-            //    var thisclothesmatch = new CodeMatch[]
-            //        {
-            //            new CodeMatch(new OpCode?(OpCodes.Stelem_Ref)),
-            //            new CodeMatch(new OpCode?(OpCodes.Ldarg_0)),
-            //            new CodeMatch(new OpCode?(OpCodes.Ldfld), anonType.GetField("asyncFlags", AccessTools.all), null),
-            //            new CodeMatch(new OpCode?(OpCodes.Brfalse)),
-            //            new CodeMatch(new OpCode?(OpCodes.Ldarg_0)),
-            //            new CodeMatch(new OpCode?(OpCodes.Ldnull)),
-            //            new CodeMatch(new OpCode?(OpCodes.Stfld)),
-            //        };
-            //    var Matcher = new CodeMatcher(instructions, null).MatchForward(false, thisclothesmatch).Advance(1);//.MatchForward(false, thisclothesmatch).Advance(2);
-
-            //    //var thisclothesmatch = new CodeMatch[]
-            //    //    {
-            //    //        new CodeMatch(new OpCode?(OpCodes.Br)),
-            //    //        new CodeMatch(new OpCode?(OpCodes.Ldarg_0)),
-            //    //        new CodeMatch(new OpCode?(OpCodes.Ldfld), anonType.GetField("$this",AccessTools.all), null),
-            //    //        new CodeMatch(new OpCode?(OpCodes.Call), AccessTools.PropertyGetter(typeof(ChaInfo), nameof(ChaInfo.objClothes))),
-            //    //        new CodeMatch(new OpCode?(OpCodes.Ldarg_0)),
-            //    //    };
-            //    //var Matcher = new CodeMatcher(instructions, null).MatchForward(true, thisclothesmatch).MatchForward(false, thisclothesmatch).Advance(2);
-
-
-            //    logger.LogWarning($"{Matcher.Pos}:\t found match");
-
-            //    var test = Matcher.InsertAndAdvance(insertInstructions).InstructionEnumeration();
-            //    for (var i = 250; i < test.Count(); i++)
-            //    {
-            //        var instrct = test.ElementAt(i);
-            //        logger.LogWarning($"{i}:\t{instrct.opcode} {instrct.operand}");
-            //    }
-            //    return test;
-            //}
-
-            //internal static void LoadFbx(ChaControl control, int category, int id, string createName, bool copyDynamicBone, byte copyWeights, Transform trfParent, int defaultId, bool asyncFlags, bool worldPositionStays, int kindNo)
-            //{
-            //    if (!control.objClothes[kindNo] && !control.hiPoly)
-            //    {
-            //        logger.LogWarning("attempting highpoly");
-            //        if (false)
-            //        {
-            //            var cor = control.LoadCharaFbxDataAsync(delegate (GameObject o)
-            //            {
-            //                control.objClothes[kindNo] = o;
-            //            }, true, category, id, createName, copyDynamicBone, copyWeights, trfParent, defaultId, worldPositionStays);
-            //            //yield return control.StartCoroutine(cor);
-            //        }
-            //        else
-            //        {
-            //            control.objClothes[kindNo] = control.LoadCharaFbxData(true, category, id, createName, copyDynamicBone, copyWeights, trfParent, defaultId, worldPositionStays);
-            //        }
-            //        //if (control.objClothes[kindNo]) control.hiPoly = true;
-            //        if (control.objClothes[kindNo] && !QueuedControls.Contains(control))
-            //        {
-            //            IEnumerator ExecuteDelayed_Routine()
-            //            {
-            //                for (var j = 0; j < 20; j++)
-            //                    yield return null;
-            //                logger.LogWarning("Attempting reload");
-            //                control.loadEnd = false;
-            //                control.hiPoly = true;
-            //                //control.ReleaseAll();
-            //                for (var j = 0; j < 20; j++)
-            //                    yield return null;
-            //                //control.StartCoroutine(control.ReloadAsync(false, false, false, false, true));
-            //                control.StartCoroutine(control.LoadAsync(true, true));
-            //                for (var j = 0; j < 20; j++)
-            //                    yield return null;
-            //                control.SetActiveTop(true);
-            //                var test = Singleton<Manager.Game>.Instance;
-            //                test.advAnimePack.SetDefalut(control);
-            //                QueuedControls.Remove(control);
-            //            }
-            //            QueuedControls.Add(control);
-            //            Instance.StartCoroutine(ExecuteDelayed_Routine());
-            //        }
-            //    }
-            //}
-
-            //internal static void LoadFbxDual(ChaControl control, int category1, int category2, int id, string createName, bool copyDynamicBone, byte copyWeights, Transform trfParent, int defaultId, bool asyncFlags, bool worldPositionStays, int kindNo) => LoadFbx(control, (control.sex != 0 || control.exType != 1) ? category1 : category2, id, createName, copyDynamicBone, copyWeights, trfParent, defaultId, asyncFlags, worldPositionStays, kindNo);
-            #endregion
         }
     }
 }
--- src/ForceHighPoly.Core/ForceHighPoly.cs
+++ src/ForceHighPoly.Core/ForceHighPoly.cs
@@ -15,17 +15,50 @@ namespace KK_Plugins
         public const string PluginName = "Force High Poly";
         public const string PluginNameInternal = Constants.Prefix + "_ForceHighPoly";
         public const string Version = "1.2.2";
-
         public static ConfigEntry<bool> Enabled { get; private set; }
-        public static ConfigEntry<bool> PartialPoly { get; private set; }
-
+        public static ConfigEntry<PolyMode> PolySetting { get; private set; }
         internal void Main()
         {
             var hasEnoughRam = KKAPI.Utilities.MemoryInfo.GetCurrentStatus().ullTotalPhys > 16L * 1000L * 1000L * 1000L; // At least 16GB
-            Enabled = Config.Bind("Config", "High poly mode", hasEnoughRam, "Whether or not to load high poly assets. Improves quality of characters in roaming mode and fixes some modded items not appearing.\nMay require exiting to main menu to take effect. This option has high memory requirements, at least 8GB of RAM is recommended (with ~10 characters).");
-            PartialPoly = Config.Bind("Config", "Partial High poly mode", true, "Use High Poly Assets on Characters who wear clothing that lack low poly models and prevents the resulting errors.\nMay require exiting to main menu to take effect. This option has Lower memory requirements, at least 8GB of RAM is recommended (with ~10 characters).");
-
+            PolySetting = Config.Bind("Config", "Set High Poly Mode State", hasEnoughRam ? PolyMode.Full : PolyMode.Partial, "Whether or not to load high poly assets. Improves quality of characters in roaming mode and fixes some modded items not appearing.\nMay require exiting to main menu to take effect.\nNone: disable plugin\n\tSome characters may throw errors and walk around without clothing.\nPartial: Loads Characters with High Poly if clothing lack a low poly model\n\tLower Requirments than Full.\nFull: This option has high memory requirements, at least 8GB of RAM is recommended (with ~10 characters).");
+            PolySetting.SettingChanged += PolySetting_SettingChanged;
+            Enabled = Config.Bind("Config", "High poly mode", hasEnoughRam, "Original Setting: kept due to externnal dependency\nWhether or not to load high poly assets. Improves quality of characters in roaming mode and fixes some modded items not appearing.\nMay require exiting to main menu to take effect. This option has high memory requirements, at least 8GB of RAM is recommended (with ~10 characters).");
+            Enabled.SettingChanged += Enabled_SettingChanged;
             HarmonyLib.Harmony.CreateAndPatchAll(typeof(Hooks));
+        }
+
+
+        private static bool LoopCheck = false;// stops having to click partial twice when swapping out
+
+        /// <summary>
+        /// Update PolySetting when setting is changed.
+        /// </summary>
+        private void Enabled_SettingChanged(object sender, System.EventArgs e)
+        {
+            if (LoopCheck) return;
+            LoopCheck = true;
+            PolySetting.Value = (Enabled.Value) ? PolyMode.Full : PolyMode.None;
+            Config.Save();
+            LoopCheck = false;
+        }
+
+        /// <summary>
+        /// Update Enabled when setting is changed. To maintain compatiility with other plugins
+        /// </summary>
+        private void PolySetting_SettingChanged(object sender, System.EventArgs e)
+        {
+            if (LoopCheck) return;
+            LoopCheck = true;
+            Enabled.Value = PolySetting.Value == PolyMode.Full;
+            Config.Save();
+            LoopCheck = false;
+        }
+
+        public enum PolyMode
+        {
+            None,
+            Partial,
+            Full
         }
     }
 }
