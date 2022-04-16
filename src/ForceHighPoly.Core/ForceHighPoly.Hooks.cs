0cbf687004a5a8e7d335e60834296808dda35f0a
jalil49 <63772680+jalil49@users.noreply.github.com>
2022-04-16 15:15:14 -0500

    Added Partial High Poly mode (#132)
    Enables high poly on a per-character basis
    It is enabled when there is a clothing piece that lacks a low poly counterpart.
    By extension preventing errors of people who lack certain pieces throwing errors

--- src/ForceHighPoly.Core/ForceHighPoly.Hooks.cs
+++ src/ForceHighPoly.Core/ForceHighPoly.Hooks.cs
@@ -1,5 +1,4 @@
 ﻿using HarmonyLib;
-using System;
 
 namespace KK_Plugins
 {
@@ -7,29 +6,91 @@ namespace KK_Plugins
     {
         internal static class Hooks
         {
-            [HarmonyPrefix, HarmonyPatch(typeof(AssetBundleManager), nameof(AssetBundleManager.LoadAsset), typeof(string), typeof(string), typeof(Type), typeof(string))]
-            private static void LoadAssetPrefix(ref string assetName) => RemoveLow(ref assetName);
-
-            [HarmonyPrefix, HarmonyPatch(typeof(AssetBundleManager), nameof(AssetBundleManager.LoadAssetAsync), typeof(string), typeof(string), typeof(Type), typeof(string))]
-            private static void LoadAssetAsyncPrefix(ref string assetName) => RemoveLow(ref assetName);
-
             /// <summary>
-            /// Strip "_low" from the end of an asset name
+            /// Test all coordiantes parts to check if a low poly doesn't exist.
+            /// if low poly doesnt exist for an item set HiPoly to true and exit;
             /// </summary>
-            private static void RemoveLow(ref string assetName)
+            /// <param name="__instance"></param>
+            [HarmonyPostfix, HarmonyPatch(typeof(ChaControl), nameof(ChaControl.Initialize))]
+            internal static void CheckHiPoly(ChaControl __instance)
             {
-                if (Enabled.Value)
+                if (PolySetting.Value == PolyMode.Full) __instance.hiPoly = true;
+
+                if (__instance.hiPoly || PolySetting.Value != PolyMode.Partial) return;
+
+                var exType = Traverse.Create(__instance).Property("exType");
+                var exTypeExists = exType.PropertyExists();
+                var coordinate = __instance.chaFile.coordinate;
+                for (var i = 0; i < coordinate.Length; i++)
                 {
-                    if (assetName.EndsWith("_low"))
+                    var clothParts = coordinate[i].clothes.parts;
+                    for (var j = 0; j < clothParts.Length; j++)
                     {
-#if KKS
-                        if (assetName != "p_cf_body_00_hit_low") //Loading the high poly version of this breaks special H modes
-#endif
+                        if (clothParts[j].id < 10000000) continue;
+                        var category = 105;
+                        switch (j)
                         {
-                            assetName = assetName.Substring(0, assetName.Length - 4);
+                            case 0:
+                                category = (__instance.sex != 0 || exTypeExists && exType.GetValue<int>() != 1) ? 105 : 503;
+                                break;
+                            case 7:
+                            case 8:
+                                category = ((__instance.sex != 0 || exTypeExists && exType.GetValue<int>() != 1) ? 112 : 504) - j;
+                                break;
+                            default:
+                                break;
+                        }
+                        category += j;
+                        var work = __instance.lstCtrl.GetCategoryInfo((ChaListDefine.CategoryNo)category);
+                        if (!work.TryGetValue(clothParts[j].id, out var lib))
+                        {
+                            continue;
+                        }
+                        else if (category == 105 || category == 107)
+                        {
+                            var infoInt = lib.GetInfoInt(ChaListDefine.KeyType.Sex);
+                            if (__instance.sex == 0 && infoInt == 3 || __instance.sex == 1 && infoInt == 2)
+                            {
+                                if (clothParts[j].id != 0)
+                                {
+                                    work.TryGetValue(0, out lib);
+                                }
+                                if (lib == null)
+                                {
+                                    continue;
+                                }
+                            }
+                        }
+                        var highAssetName = lib.GetInfo(ChaListDefine.KeyType.MainData);
+                        if (string.Empty == highAssetName)
+                        {
+                            continue;
+                        }
+                        var manifestName = lib.GetInfo(ChaListDefine.KeyType.MainManifest);
+                        var assetBundleName = lib.GetInfo(ChaListDefine.KeyType.MainAB);
+#if KK
+                        Singleton<Manager.Character>.Instance.AddLoadAssetBundle(assetBundleName, manifestName);
+#elif KKS
+                        Manager.Character.AddLoadAssetBundle(assetBundleName, manifestName);
+#endif
+                        if (!CommonLib.LoadAsset<UnityEngine.GameObject>(assetBundleName, highAssetName + "_low", false, manifestName) && CommonLib.LoadAsset<UnityEngine.GameObject>(assetBundleName, highAssetName, false, manifestName))
+                        {
+                            __instance.hiPoly = true;
+                            return;
                         }
                     }
                 }
+            }
+
+            /// <summary>
+            /// Might not be neccessary because of the first line of the above method, put probably also cheaper than the previous method
+            /// Equivilant to last method results of patching AssetBundleManager and trimming _low
+            /// </summary>
+            /// <param name="__result"></param>
+            [HarmonyPostfix, HarmonyPatch(typeof(ChaInfo), nameof(ChaInfo.hiPoly), MethodType.Getter)]
+            private static void HiPolyPostfix(ref bool __result)
+            {
+                __result = __result || Enabled.Value;
             }
         }
     }
--- src/ForceHighPoly.Core/ForceHighPoly.cs
+++ src/ForceHighPoly.Core/ForceHighPoly.cs
@@ -1,6 +1,5 @@
 ﻿using BepInEx;
 using BepInEx.Configuration;
-using HarmonyLib;
 using KKAPI;
 
 namespace KK_Plugins
@@ -16,15 +15,50 @@ namespace KK_Plugins
         public const string PluginName = "Force High Poly";
         public const string PluginNameInternal = Constants.Prefix + "_ForceHighPoly";
         public const string Version = "1.2.2";
-
         public static ConfigEntry<bool> Enabled { get; private set; }
-
+        public static ConfigEntry<PolyMode> PolySetting { get; private set; }
         internal void Main()
         {
             var hasEnoughRam = KKAPI.Utilities.MemoryInfo.GetCurrentStatus().ullTotalPhys > 16L * 1000L * 1000L * 1000L; // At least 16GB
-            Enabled = Config.Bind("Config", "High poly mode", hasEnoughRam, "Whether or not to load high poly assets. Improves quality of characters in roaming mode and fixes some modded items not appearing.\nMay require exiting to main menu to take effect. This option has high memory requirements, at least 8GB of RAM is recommended (with ~10 characters).");
+            PolySetting = Config.Bind("Config", "Force High Poly Models", hasEnoughRam ? PolyMode.Full : PolyMode.Partial, "Whether or not to load high poly models in the main game roaming mode (has no effect outside of that). Improves quality of characters and fixes some modded items not appearing.\nMay require exiting to main menu to take effect.\nNone: Original behavior - Some characters may throw errors and walk around with glitched clothing.\nPartial: Use high poly models only if necessary. Higher resource usage and longer load times when using some modded clothes.\nFull: Always use high poly models. Best quality but very high resource usage. At least 8GB of RAM is recommended with ~10 characters. Load times can be 3x longer.");
+            PolySetting.SettingChanged += PolySetting_SettingChanged;
+            Enabled = Config.Bind("Config", "High poly mode", hasEnoughRam, new ConfigDescription("Original setting kept due to external dependency, no longer used.\nUse the 'Force High Poly Models' setting instead.", null, new ConfigurationManagerAttributes() { Browsable = false }));
+            Enabled.SettingChanged += Enabled_SettingChanged;
+            HarmonyLib.Harmony.CreateAndPatchAll(typeof(Hooks));
+        }
 
-            Harmony.CreateAndPatchAll(typeof(Hooks));
+
+        private static bool LoopCheck = false;// stops having to click partial twice when swapping out
+
+        /// <summary>
+        /// Update PolySetting when setting is changed.
+        /// </summary>
+        private void Enabled_SettingChanged(object sender, System.EventArgs e)
+        {
+            if (LoopCheck) return;
+            LoopCheck = true;
+            PolySetting.Value = (Enabled.Value) ? PolyMode.Full : PolyMode.None;
+            Config.Save();
+            LoopCheck = false;
+        }
+
+        /// <summary>
+        /// Update Enabled when setting is changed. To maintain compatiility with other plugins
+        /// </summary>
+        private void PolySetting_SettingChanged(object sender, System.EventArgs e)
+        {
+            if (LoopCheck) return;
+            LoopCheck = true;
+            Enabled.Value = PolySetting.Value == PolyMode.Full;
+            Config.Save();
+            LoopCheck = false;
+        }
+
+        public enum PolyMode
+        {
+            None,
+            Partial,
+            Full
         }
     }
 }
