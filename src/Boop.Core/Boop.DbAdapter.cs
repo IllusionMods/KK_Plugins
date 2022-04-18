27b7d738f1a416aaedcff473d75fbc766c6d9d44
ManlyMarco <39247311+ManlyMarco@users.noreply.github.com>
2022-04-18 21:49:08 +0200

    v2

---
+++ src/Boop.Core/Boop.DbAdapter.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Linq;
+using UnityEngine;
+
+namespace KK_Plugins
+{
+    public partial class Boop
+    {
+        public sealed class DbAdapter
+        {
+            public readonly MonoBehaviour BoneMb;
+            public readonly Func<Transform> GetTransform;
+
+            private readonly Func<Vector3> _getForce;
+            private readonly Action<Vector3> _setForce;
+            private Vector3? _originalForce;
+
+            public Vector3 GetForce() => _getForce();
+
+            public void SetForce(Vector3 f)
+            {
+                if (!_originalForce.HasValue) _originalForce = _getForce();
+                _setForce(f);
+            }
+
+            public void ResetForce()
+            {
+                if (_originalForce.HasValue)
+                {
+                    SetForce(_originalForce.Value);
+                    _originalForce = null;
+                }
+            }
+
+            public void ApplyForce(Vector3 f)
+            {
+                SetForce((GetForce() + -f * _scaling.Value) * _damping.Value);
+            }
+
+            private DbAdapter(MonoBehaviour boneMb, Func<Transform> getTransform, Func<Vector3> getForce, Action<Vector3> setForce)
+            {
+                BoneMb = boneMb;
+                GetTransform = getTransform;
+                _getForce = getForce;
+                _setForce = setForce;
+            }
+
+            public static DbAdapter Create(DynamicBone bone)
+            {
+                return new DbAdapter(bone, () => bone.m_Particles.Last(t => t?.m_Transform != null).m_Transform, () => bone.m_Force, f => bone.m_Force = f);
+            }
+            public static DbAdapter Create(DynamicBone_Ver01 bone)
+            {
+                return new DbAdapter(bone, () => bone.m_Particles.Last(t => t?.m_Transform != null).m_Transform, () => bone.m_Force, f => bone.m_Force = f);
+            }
+            public static DbAdapter Create(DynamicBone_Ver02 bone)
+            {
+                return new DbAdapter(bone, () => bone.Bones.Last(t => t != null), () => bone.Force, f => bone.Force = f);
+            }
+
+            public static DbAdapter Create(MonoBehaviour bone)
+            {
+                if (bone == null) throw new ArgumentNullException(nameof(bone));
+                switch (bone)
+                {
+                    case DynamicBone db: return Create(db);
+                    case DynamicBone_Ver01 db1: return Create(db1);
+                    case DynamicBone_Ver02 db2: return Create(db2);
+                    default: throw new ArgumentException("unknown type " + bone.GetType());
+                }
+            }
+        }
+    }
+}
--- src/Boop.Core/Boop.cs
+++ src/Boop.Core/Boop.cs
@@ -1,103 +1,91 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using BepInEx;
+using BepInEx.Configuration;
+using HarmonyLib;
 using KKAPI;
-using KKAPI.Chara;
+using KKAPI.Studio;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 namespace KK_Plugins
 {
     [BepInPlugin(GUID, PluginName, Version)]
     [BepInDependency(KoikatuAPI.GUID, KoikatuAPI.VersionConst)]
-    public class Boop : BaseUnityPlugin
+    public partial class Boop : BaseUnityPlugin
     {
         public const string GUID = "Boop";
         public const string PluginName = "Boop";
         internal const string PluginNameInternal = "Boop";
-        public const string Version = "1.0.1";
+        public const string Version = "2.0";
 
-        private static float _damping = 0.5f;
-        private static int _distance = 100;
-        private static float _scaling = 0.0002f;
+        private static ConfigEntry<float> _damping;
+        private static ConfigEntry<int> _distance;
+        private static ConfigEntry<float> _scaling;
 
-        private static readonly List<ChaInfo> _ChaInfos = new List<ChaInfo>();
-        private static IEnumerable<DynamicBone_Ver02> _dbv2S;
-
-        private Camera _main;
+        private Camera _mainCam;
         private Vector3 _mousePosPrev = Vector3.zero;
         private readonly CircularBuffer _mouseVelocity = new CircularBuffer(5);
+        private static readonly List<DbAdapter> _DBs = new List<DbAdapter>();
 
         private void Awake()
         {
-            CharacterApi.RegisterExtraBehaviour<BoopController>(GUID);
-            SceneManager.sceneLoaded += SceneManager_sceneLoaded;
-        }
+            _distance = Config.Bind("Mouse booping", "Distance", 100);
+            _damping = Config.Bind("Mouse booping", "Damping", 0.5f);
+            _scaling = Config.Bind("Mouse booping", "Scaling", 0.0002f);
 
-        private static void SceneManager_sceneLoaded(Scene scene, LoadSceneMode lsm)
-        {
-            try
+            if (!Config.Bind("General", "Run in Studio", true).Value && StudioAPI.InsideStudio)
             {
-                RefreshDynamicBones();
-            }
-            catch (Exception e)
-            {
-                Debug.LogException(e);
-            }
-        }
-
-        public static void RefreshDynamicBones()
-        {
-            for (var i = _ChaInfos.Count - 1; i >= 0; i--)
-            {
-                if (_ChaInfos[i] == null)
-                    _ChaInfos.RemoveAt(i);
+                enabled = false;
+                return;
             }
 
-            _dbv2S = _ChaInfos.SelectMany(x => x.GetComponentsInChildren<DynamicBone_Ver02>());
-            foreach (var db in _dbv2S) db.Force = Vector3.zero;
+            Harmony.CreateAndPatchAll(typeof(Boop), GUID);
+
+#if DEBUG // for hot reloading
+            foreach (var db in FindObjectsOfType<DynamicBone>()) OnDynamicBoneInit(db);
+            foreach (var db in FindObjectsOfType<DynamicBone_Ver01>()) OnDynamicBoneInit(db);
+            foreach (var db in FindObjectsOfType<DynamicBone_Ver02>()) OnDynamicBoneInit(db);
+#endif
         }
 
-        public static void RegisterChar(ChaInfo ci)
+        [HarmonyPostfix]
+        [HarmonyWrapSafe]
+        [HarmonyPatch(typeof(DynamicBone), nameof(DynamicBone.SetupParticles))]
+        [HarmonyPatch(typeof(DynamicBone_Ver01), nameof(DynamicBone_Ver01.SetupParticles))]
+        [HarmonyPatch(typeof(DynamicBone_Ver02), nameof(DynamicBone_Ver02.SetupParticles))]
+        private static void OnDynamicBoneInit(MonoBehaviour __instance)
         {
-            _ChaInfos.Add(ci);
-            RefreshDynamicBones();
-        }
-
-        public static void UnregisterChar(ChaInfo ci)
-        {
-            if (_ChaInfos.Remove(ci)) RefreshDynamicBones();
-        }
-
-        private static void ApplyForce(DynamicBone_Ver02 dbc, Vector3 f)
-        {
-            dbc.Force += -f * _scaling;
+            _DBs.Add(DbAdapter.Create(__instance));
         }
 
         private void Update()
         {
-            if (_main == null) _main = Camera.main;
-            if (_main == null) return;
+            if (_mainCam == null) _mainCam = Camera.main;
+            if (_mainCam == null) return;
 
             var mousePosition = Input.mousePosition;
             var obj = _mousePosPrev - mousePosition;
             _mouseVelocity.Add(obj);
 
-            if (_dbv2S != null)
+            _mousePosPrev = mousePosition;
+            var point = _mouseVelocity.Average();
+            var f = _mainCam.transform.rotation * point;
+
+            for (var index = 0; index < _DBs.Count; index++)
             {
-                _mousePosPrev = mousePosition;
-                var point = _mouseVelocity.Average();
-                var f = _main.transform.rotation * point;
-
-                foreach (var db in _dbv2S)
+                var db = _DBs[index];
+                if (db.BoneMb == null)
                 {
-                    var boneDist = Vector3.Distance(_main.transform.position, db.Bones.Last().transform.position);
-                    if (Vector3.Distance(_main.WorldToScreenPoint(db.Bones.Last().transform.position), mousePosition) < _distance / boneDist)
-                        ApplyForce(db, f);
-
-                    db.Force *= _damping;
+                    _DBs.RemoveAt(index);
+                    index--;
+                    continue;
                 }
+
+                var bonePos = db.GetTransform().position;
+                var boneDist = Vector3.Distance(_mainCam.transform.position, bonePos);
+                if (Vector3.Distance(_mainCam.WorldToScreenPoint(bonePos), mousePosition) < _distance.Value / boneDist)
+                    db.ApplyForce(f);
+                else
+                    db.ResetForce();
             }
         }
     }
