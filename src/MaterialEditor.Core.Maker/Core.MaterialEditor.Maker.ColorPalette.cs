2ab8b9dda133df0bec90d99cd058a7656303b465
AgiShark <86574514+AgiShark@users.noreply.github.com>
2022-08-23 03:12:55 +0900

    Add a color picker for color properties of PH_ME (#153)
    * Add a color picker for color properties of PH_ME
    
    * Apply IDE suggestions

--- src/MaterialEditor.Core.Maker/Core.MaterialEditor.Maker.ColorPalette.cs
+++ src/MaterialEditor.Core.Maker/Core.MaterialEditor.Maker.ColorPalette.cs
@@ -13,10 +13,70 @@ namespace KK_Plugins.MaterialEditor
 #if PH
     internal class MakerColorPalette : IMaterialEditorColorPalette
     {
-        public void Close() { }
-        public bool IsShowing(string title, object data, string materialName) => false;
-        public void SetColor(Color value) { }
-        public void Setup(string title, object data, string materialName, Color color, Action<Color> onChanged, bool useAlpha) { }
+        private int _slot = -1;
+        private string _materialName;
+        //Normal Maker mode
+        private MoveableUI _mui;
+        private MoveableUI MUI => _mui != null ? _mui : (_mui = GameObject.Find("EditScene/Canvas/MoveableRoot/ColorUI").GetComponent<MoveableUI>());
+        private MoveableColorCustomUI _movcui;
+        private MoveableColorCustomUI MCCUI => _movcui != null ? _movcui : (_movcui = GameObject.Find("EditScene/Canvas/MoveableRoot/ColorUI").GetComponent<MoveableColorCustomUI>());
+        //H-Scene Maker mode
+        private readonly H_Scene hscene = UnityEngine.Object.FindObjectOfType<H_Scene>();
+        private MoveableUI _mui_h;
+        private MoveableUI MUI_H => _mui_h != null ? _mui_h : (_mui_h = GameObject.Find("Left Middle Canvas/MoveableRoot/ColorUI").GetComponent<MoveableUI>());
+        private MoveableColorCustomUI _movcui_h;
+        private MoveableColorCustomUI MCCUI_H => _movcui_h != null ? _movcui_h : (_movcui_h = GameObject.Find("Left Middle Canvas/MoveableRoot/ColorUI").GetComponent<MoveableColorCustomUI>());
+
+        public void Close()
+        {
+            var mccui_all = (hscene != null) ? MCCUI_H : MCCUI;
+
+            if (mccui_all.isOpen)
+            {
+                mccui_all.Close();
+            }
+            _slot = -1;
+            _materialName = null;
+        }
+
+        public bool IsShowing(string title, object data, string materialName)
+        {
+            var mccui_all = (hscene != null) ? MCCUI_H : MCCUI;
+            var mui_all = (hscene != null) ? MUI_H : MUI;
+
+            return
+            mccui_all.isOpen
+            && mui_all.title.text == title
+            && _slot == ((ObjectData)data).Slot
+            && _materialName == materialName;
+        }
+
+        public void SetColor(Color value)
+        {
+            var mccui_all = (hscene != null) ? MCCUI_H : MCCUI;
+
+            mccui_all.color = value;
+        }
+
+        public void Setup(string title, object data, string materialName, Color color, Action<Color> onChanged, bool useAlpha)
+        {
+            var mccui_all = (hscene != null) ? MCCUI_H : MCCUI;
+            var mui_all = (hscene != null) ? MUI_H : MUI;
+
+            mui_all.SetTitle(title);
+            mui_all.Open();
+            mccui_all.colorPicker.Setup(color, useAlpha, onChanged);
+
+            //* Turning off lists with Thumbnails.
+            //MoveableThumbnailSelectUI[] array = UnityEngine.Object.FindObjectsOfType<MoveableThumbnailSelectUI>();
+            //for (int i = 0; i < array.Length; i++)
+            //{
+            //    array[i].Close();
+            //}
+
+            _slot = ((ObjectData)data).Slot;
+            _materialName = materialName;
+        }
     }
 #elif KK || EC || KKS
     internal class MakerColorPalette : IMaterialEditorColorPalette
--- src/MaterialEditor.Core.Studio/Core.MaterialEditor.Studio.ColorPalette.cs
+++ src/MaterialEditor.Core.Studio/Core.MaterialEditor.Studio.ColorPalette.cs
@@ -1,16 +1,65 @@
 ﻿using MaterialEditorAPI;
 using System;
 using UnityEngine;
+#if PH
+using Studio;
+#endif
 
 namespace KK_Plugins.MaterialEditor
 {
 #if PH
     internal class StudioColorPalette : IMaterialEditorColorPalette
     {
-        public void Close() { }
-        public bool IsShowing(string title, object data, string materialName) => false;
-        public void SetColor(Color value) { }
-        public void Setup(string title, object data, string materialName, Color color, Action<Color> onChanged, bool useAlpha) { }
+        private int _slot = -1;
+        private string _materialName;
+        private UI_ColorInfo.UpdateColor _onChanged = null;
+
+        private Studio.Studio Studio => Singleton<Studio.Studio>.Instance;
+
+        public void Close()
+        {
+            Studio.colorPaletteCtrl.visible = false;
+            Studio.colorMenu.winTitle.text = "Color"; //Set as default name.
+        }
+
+        public bool IsShowing(string title, object data, string materialName)
+        {
+            return
+                   Studio.colorMenu.winTitle.text == title
+                   && Studio.colorPaletteCtrl.visible == true
+                   && _slot == DataToSlot(data)
+                   && _materialName == materialName;
+        }
+
+        public void SetColor(Color value)
+        {
+            Studio.colorMenu.SetColor(value, UI_ColorInfo.ControlType.PresetsSample);
+        }
+
+        public void Setup(string title, object data, string materialName, Color color, Action<Color> onChanged, bool useAlpha)
+        {
+            if (Studio.colorMenu.updateColorFunc == _onChanged)
+                Studio.colorPaletteCtrl.visible = !Studio.colorPaletteCtrl.visible;
+            else
+                Studio.colorPaletteCtrl.visible = true;
+            if (Studio.colorPaletteCtrl.visible)
+            {
+                Studio.colorMenu.winTitle.text = title;
+                Studio.colorMenu.updateColorFunc = _onChanged;
+                Studio.colorMenu.updateColorFunc = new UI_ColorInfo.UpdateColor(onChanged);
+                Studio.colorMenu.SetColor(color, UI_ColorInfo.ControlType.PresetsSample);
+            }
+            _slot = DataToSlot(data);
+            _materialName = materialName;
+        }
+
+        private static int DataToSlot(object data)
+        {
+            if (data is ObjectData objectData) //Character
+                return objectData.Slot;
+            else //Item
+                return (int)data;
+        }
     }
 #else
     internal class StudioColorPalette : IMaterialEditorColorPalette
