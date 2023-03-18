0a678e91f49f1240c6713c1fccbc3da825d0a2e6
ManlyMarco <39247311+ManlyMarco@users.noreply.github.com>
2023-03-18 19:32:54 +0100

    Include US zipmods in releases created by release script

--- release.ps1
+++ release.ps1
@@ -1,6 +1,6 @@
 $array = @("KK", "EC", "AI", "HS", "HS2", "PH", "PC", "KKS", "SBPR")
 $dir = $PSScriptRoot + "/bin/"
-$copy = $dir + "/copy/BepInEx"
+$copy = $dir + "/copy"
 
 Remove-Item -Force -Path ($dir + "/copy") -Recurse -ErrorAction SilentlyContinue
 Remove-Item -Force -Path ($dir + "/out") -Recurse -ErrorAction SilentlyContinue
@@ -16,21 +16,31 @@ foreach ($element in $array)
 		if ($filename.StartsWith($element)) 
 		{
 			Remove-Item -Force -Path ($copy) -Recurse
-			New-Item -ItemType Directory -Force -Path ($copy + "/plugins/" + $element + "_Plugins/")
+			New-Item -ItemType Directory -Force -Path ($copy + "/BepInEx/plugins/" + $element + "_Plugins/")
 			
-			Copy-Item -Path ($filepath) -Destination ($copy + "/plugins/" + $element + "_Plugins/") -Force
+			Copy-Item -Path ($filepath) -Destination ($copy + "/BepInEx/plugins/" + $element + "_Plugins/") -Force
+
+            foreach ($modPath in [System.IO.Directory]::EnumerateFiles($dir + "..\mods\","*.zipmod","AllDirectories"))
+            {
+                $modfilename = $modPath.Replace($dir + "..\mods\", "")
+                if($modfilename.StartsWith($filename.Replace(".dll", "")))
+                {
+			        New-Item -ItemType Directory -Force -Path ($copy + "/mods")
+			        Copy-Item -Path ($modPath) -Destination ($copy + "/mods/") -Force
+                }
+            }
 			
 			$filepathxml = get-childitem $dir -Recurse -Force -include $filename.Replace(".dll", ".xml") -ErrorAction SilentlyContinue
 			if ($filepathxml)
 			{
-				Copy-Item -Path ($filepathxml) -Destination ($copy + "/plugins/" + $element + "_Plugins/") -Force
+				Copy-Item -Path ($filepathxml) -Destination ($copy + "/BepInEx/plugins/" + $element + "_Plugins/") -Force
 			}
 			
 			$version = "v" + (Get-ChildItem -Path ($filepath) -Filter "*.dll" -Force)[0].VersionInfo.FileVersion.ToString()
 			$zipfilename = $filename.Replace(".dll", " " + $version + ".zip")
 			
 			"Creating archive: " + $zipfilename
-			Compress-Archive -Path $copy -Force -CompressionLevel "Optimal" -DestinationPath ($dir + "out/" + $zipfilename)
+			Compress-Archive -Path ($copy + "\*") -Force -CompressionLevel "Optimal" -DestinationPath ($dir + "out/" + $zipfilename)
 		}
 	}
 }
