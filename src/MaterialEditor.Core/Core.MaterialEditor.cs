64ee79db3b60e8499d79c975e41790cd19c2351f
DeathWeasel1337 <deathweasel1337@gmail.com>
2023-08-22 19:02:55 -0500

    Update Core.MaterialEditor.cs
    MaterialEditor version to 3.1.24

--- src/MaterialEditor.Core/Core.MaterialEditor.cs
+++ src/MaterialEditor.Core/Core.MaterialEditor.cs
@@ -58,7 +58,7 @@ namespace KK_Plugins.MaterialEditor
         /// <summary>
         /// MaterialEditor plugin version
         /// </summary>
-        public const string PluginVersion = "3.1.23";
+        public const string PluginVersion = "3.1.24";
 
         /// <summary>
         /// Material which is used in normal map conversion
