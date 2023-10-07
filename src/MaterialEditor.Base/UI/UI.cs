6d217e93a68b18d16f80d9b4b250eff054516981
RikkiBalboa <BalboaRikki+github@protonmail.com>
2023-10-07 20:04:30 +0200

    initial ME timeline compatibility test

--- src/MaterialEditor.Base/UI/UI.cs
+++ src/MaterialEditor.Base/UI/UI.cs
@@ -1,9 +1,16 @@
 ﻿using BepInEx;
+
+#if !PH && !AI && !EC && !API
+using KKAPI.Studio;
+using KKAPI.Utilities;
+using Studio;
+#endif
 using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Xml;
 using UILib;
 using UnityEngine;
 using UnityEngine.UI;
@@ -248,6 +255,62 @@ namespace MaterialEditorAPI
                 items.Add(rendererItem);
 
                 //Renderer Enabled
+#if !PH && !AI && !EC && !API
+                TimelineCompatibility.AddInterpolableModelDynamic(
+                    owner: "MaterialEditor",
+                    id: "targetEnabled",
+                    name: "Enabled",
+                    interpolateBefore: (oci, parameter, leftValue, rightValue, factor) => ((Renderer)parameter).enabled = (bool)leftValue,
+                    interpolateAfter: null,
+                    isCompatibleWithTarget: (oci) => oci != null,
+                    getValue: (oci, parameter) => {
+                        GameObject _go = null;
+                        if (oci is OCIItem ociItem)
+                            _go = ociItem.objectItem;
+                        else if (oci is OCIChar ociChar)
+                            _go = ociChar.charInfo.gameObject;
+
+                        if (_go != null)
+                            return GetRendererList(_go).First(x => x.NameFormatted() == rend.NameFormatted()).enabled;
+                        else return true;
+                        //((Renderer)parameter).enabled = (bool)leftValue;
+                    },
+                    readValueFromXml: (parameter, node) => XmlConvert.ToBoolean(node.Attributes["value"].Value),
+                    writeValueToXml: (parameter, writer, value) => writer.WriteAttributeString("value", XmlConvert.ToString((bool)value)),
+                    getParameter: (oci) =>
+                    {
+                        GameObject _go;
+                        if (oci is OCIItem ociItem)
+                            _go = ociItem.objectItem;
+                        else if (oci is OCIChar ociChar)
+                            _go = ociChar.charInfo.gameObject;
+                        else return null;
+
+                        return GetRendererList(_go).First(x => x == rend);
+                    },
+                    readParameterFromXml: (oci, node) => 
+                    {
+                        GameObject _go = null;
+                        if (oci is OCIItem ociItem)
+                            _go = ociItem.objectItem;
+                        else if (oci is OCIChar ociChar)
+                            _go = ociChar.charInfo.gameObject;
+
+                        if (_go != null)
+                            return GetRendererList(_go).First(x => x.NameFormatted() == node.Attributes["parameter"].Value);
+                        else return null;
+                    },
+                    checkIntegrity: (oci, parameter, leftValue, rightValue) =>
+                    {
+                        if (parameter is Renderer && parameter != null)
+                            return true;
+                        return false;
+                    },
+                    writeParameterToXml: (oci, writer, parameter) => writer.WriteAttributeString("parameter", parameter.NameFormatted()),
+                    getFinalName: (currentName, oci, parameter) => $"Enabled: {parameter}"
+                );
+#endif
+
                 bool valueEnabledOriginal = rend.enabled;
                 var temp = GetRendererPropertyValueOriginal(data, rend, RendererProperties.Enabled, go);
                 if (!temp.IsNullOrEmpty())
