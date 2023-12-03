07955659d5dbd9c2c2e106470ade583b2d0fba44
ManlyMarco <39247311+ManlyMarco@users.noreply.github.com>
2023-12-03 16:05:41 +0100

    Add AccessoriesToStudioItems (new plugin) (#205)
    Plugin for studio that makes normal character accessories available as items.
    They are visible in the Item list and in QAB just like normal items.
    To see all accessories in QAB, search for ao_.
    
    Requires at least QuickAccessBox v3.1.1 and BepisPlugins r19.3.2 to work.

--- src/Shared/AssemblyInfo.cs
+++ src/Shared/AssemblyInfo.cs
@@ -3,7 +3,7 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("https://github.com/DeathWeasel1337/KK_Plugins")]
-[assembly: AssemblyCopyright("Copyright ©DeathWeasel1337 2019")]
+[assembly: AssemblyCopyright("Copyright ©DeathWeasel1337 & contributors 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
