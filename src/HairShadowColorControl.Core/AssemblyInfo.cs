8de9323a6e7e35d1f92e64e9453fab9f5efa97d2
ManlyMarco <39247311+ManlyMarco@users.noreply.github.com>
2024-01-17 23:34:54 +0100

    [KK/KKS] Add HairShadowColorControl plugin (#219)
    Convenient controls for changing the shadow color of character hair in maker. Uses ME underneath.

---
+++ src/HairShadowColorControl.Core/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+using HairShadowColorControl;
+
+[assembly: AssemblyTitle(HairShadowColorControlPlugin.GUID)]
+[assembly: AssemblyDescription(HairShadowColorControlPlugin.DisplayName)]
+[assembly: AssemblyCompany("https://github.com/IllusionMods/KK_Plugins/")]
+[assembly: AssemblyProduct(HairShadowColorControlPlugin.DisplayName)]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("0ff15905-4a4e-43ee-9853-f410f0a63876")]
+
+[assembly: AssemblyVersion(HairShadowColorControlPlugin.Version)]
