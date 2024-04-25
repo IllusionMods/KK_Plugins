704f036654996b254c66b8cca74d9f0db392a7ab
ManlyMarco <39247311+ManlyMarco@users.noreply.github.com>
2024-04-25 19:25:41 +0200

    Bump version numbers

--- src/MaterialEditor.Core/Core.MaterialEditor.cs
+++ src/MaterialEditor.Core/Core.MaterialEditor.cs
@@ -58,7 +58,7 @@ namespace KK_Plugins.MaterialEditor
         /// <summary>
         /// MaterialEditor plugin version
         /// </summary>
-        public const string PluginVersion = "3.5.0";
+        public const string PluginVersion = "3.6.0";
 
         /// <summary>
         /// Material which is used in normal map conversion
