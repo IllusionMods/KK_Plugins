38352b751f4affde46df566e9d4332b9bc6dff1d
ManlyMarco <39247311+ManlyMarco@users.noreply.github.com>
2025-07-28 03:46:43 +0200

    Update assembly metadata

--- src/Shared/AssemblyInfo.cs
+++ src/Shared/AssemblyInfo.cs
@@ -2,8 +2,8 @@
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("https://github.com/DeathWeasel1337/KK_Plugins")]
-[assembly: AssemblyCopyright("Copyright ©DeathWeasel1337 & contributors 2019")]
+[assembly: AssemblyCompany("https://github.com/IllusionMods/KK_Plugins")]
+[assembly: AssemblyCopyright("Copyright GPL-3.0 © 2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
