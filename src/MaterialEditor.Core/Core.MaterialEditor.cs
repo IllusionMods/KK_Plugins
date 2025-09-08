56b7943f34f6afcc4c1d3546e568361c0f4c8618
ManlyMarco <39247311+ManlyMarco@users.noreply.github.com>
2025-09-08 18:35:25 +0200

    Bump version numbers

--- src/MaterialEditor.Core/Core.MaterialEditor.cs
+++ src/MaterialEditor.Core/Core.MaterialEditor.cs
@@ -61,7 +61,7 @@ namespace KK_Plugins.MaterialEditor
         /// <summary>
         /// MaterialEditor plugin version
         /// </summary>
-        public const string PluginVersion = "3.13.2";
+        public const string PluginVersion = "3.13.3";
 
 
         /// <summary>
--- src/Pushup.Core/Core.Pushup.cs
+++ src/Pushup.Core/Core.Pushup.cs
@@ -23,7 +23,7 @@ namespace KK_Plugins
         public const string GUID = "com.deathweasel.bepinex.pushup";
         public const string PluginName = "Pushup";
         public const string PluginNameInternal = Constants.Prefix + "_Pushup";
-        public const string Version = "1.5.1";
+        public const string Version = "1.5.2";
         internal static new ManualLogSource Logger;
 
         private static Type MoreOutfitsType;
