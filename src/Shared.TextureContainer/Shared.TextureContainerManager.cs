04057777906290a8d8049c4841e5d98773df1340
starstormhun <markus.tamas98@gmail.com>
2025-10-21 03:27:23 +0200

    Change texture hashing to CRC64, up to 2048 bytes

--- src/Shared.TextureContainer/Shared.TextureContainerManager.cs
+++ src/Shared.TextureContainer/Shared.TextureContainerManager.cs
@@ -22,14 +22,14 @@ namespace KK_Plugins
         internal class TextureKey
         {
             public readonly byte[] data;
-            public readonly int hash;
+            public readonly long hash;
             public readonly TextureFormat format;
             public readonly bool mipmaps;
 
             public TextureKey(byte[] data, TextureFormat format = TextureFormat.ARGB32, bool mipmaps = true)
             {
                 //First part of the data is sufficient to calculate the hash.
-                int hash = (int)CRC32Calculator.CalculateCRC32(data, 1 << 10);
+                long hash = (long)CRC64Calculator.CalculateCRC64(data, 1 << 11);
                 hash ^= format.GetHashCode();
                 hash ^= mipmaps.GetHashCode();
 
@@ -55,7 +55,7 @@ namespace KK_Plugins
 
             public override int GetHashCode()
             {
-                return hash;
+                return (int)hash;
             }
         }
 
@@ -102,10 +102,7 @@ namespace KK_Plugins
         /// </summary>
         /// <param name="filePath"></param>
         /// <returns></returns>
-        public static Token Acquire(string filePath)
-        {
-            return Acquire(LoadTextureBytes(filePath));
-        }
+        public static Token Acquire(string filePath) => Acquire(LoadTextureBytes(filePath));
 
         /// <summary>
         /// Acquire TextureHolder. If it already exists, return it.
@@ -220,6 +217,82 @@ namespace KK_Plugins
             }
 
             return table;
+        }
+    }
+
+    /// <summary>
+    /// Class for calculating CRC64 from a byte array
+    /// </summary>
+    public static class CRC64Calculator
+    {
+        private const int width = 64;
+        private const ulong polynomial = 0xE4C11DB7B4AC89F3;
+        private const ulong initialValue = 0xFFFFFFFFFFFFFFFF;
+        private const ulong xorOutValue = 0xFFFFFFFFFFFFFFFF;
+
+        private static readonly ulong[] Crc64Table;
+
+        static CRC64Calculator()
+        {
+            // Initialize CRC64 table
+            Crc64Table = GenerateCrc64Table();
+        }
+
+        /// <summary>
+        /// Calculate the numeric hash
+        /// </summary>
+        /// <param name="data">The data bytes to hash.</param>
+        /// <param name="size">How many bytes to hash. Leave out to hash all.</param>
+        public static ulong CalculateCRC64(byte[] data, int? size = null)
+        {
+            size = Math.Min(data.Length, size ?? data.Length);
+            byte[] crcCheckVal = CalculateCheckValue(data, size.Value);
+            Array.Resize(ref crcCheckVal, 8);
+            return BitConverter.ToUInt64(crcCheckVal, 0);
+        }
+
+        public static byte[] CalculateCheckValue(byte[] data, int size)
+        {
+            if (data == null) return null;
+
+            ulong crc = initialValue;
+
+            for (int i = 0; i < size; i++)
+            {
+                crc = Crc64Table[((crc >> (width - 8)) ^ data[i]) & 0xFF] ^ (crc << 8);
+                crc &= UInt64.MaxValue >> (64 - width);
+            }
+
+            ulong crcFinalValue = crc ^ xorOutValue;
+            return BitConverter.GetBytes(crcFinalValue).Take((width + 7) / 8).ToArray();
+        }
+
+        private static ulong[] GenerateCrc64Table()
+        {
+            var lookupTable = new ulong[256];
+            ulong topBit = (ulong)1 << (width - 1);
+
+            for (int i = 0; i < lookupTable.Length; i++)
+            {
+                byte inByte = (byte)i;
+
+                ulong r = (ulong)inByte << (width - 8);
+                for (int j = 0; j < 8; j++)
+                {
+                    if ((r & topBit) != 0)
+                    {
+                        r = (r << 1) ^ polynomial;
+                    }
+                    else
+                    {
+                        r <<= 1;
+                    }
+                }
+
+                lookupTable[i] = r & (UInt64.MaxValue >> (64 - width));
+            }
+
+            return lookupTable;
         }
     }
 }
